8d8ca724ce14fc1e76a49d548bda48a5dc820ca8
Anthony Bonta <zthun.coding@gmail.com>
2023-02-25 17:27:36 -0600

    refactor: added the header

--- packages/fashion-web/src/fashion-app/fashion-app.tsx
+++ packages/fashion-web/src/fashion-app/fashion-app.tsx
@@ -1,3 +1,5 @@
+import { ZRunwayHeaderMain } from '@zthun/fashion-runway';
+import { ZCaption, ZH1 } from '@zthun/fashion-venue';
 import React from 'react';
 
 /**
@@ -7,5 +9,16 @@ import React from 'react';
  *        The jsx to render the fashion web application.
  */
 export function ZFashionApp() {
-  return <main className='ZFashionApp-root'>Fashion!</main>;
+  const avatar = <img src='images/svg/fashion.svg' />;
+  const prefix = (
+    <div className='ZFashionApp-description'>
+      <ZH1 compact>Fashion</ZH1>
+      <ZCaption compact>Welcome to the show</ZCaption>
+    </div>
+  );
+  return (
+    <ZRunwayHeaderMain avatar={avatar} prefix={prefix}>
+      Content Here!
+    </ZRunwayHeaderMain>
+  );
 }
