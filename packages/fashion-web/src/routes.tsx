6491ad0ff0920198ce9dad52b85cbb0962ca85ff
Anthony Bonta <zthun.coding@gmail.com>
2023-02-26 14:01:37 -0600

    chore: routes for web

---
+++ packages/fashion-web/src/routes.tsx
@@ -0,0 +1,39 @@
+import { ZRouteBuilder } from '@zthun/fashion-designer';
+
+export const ZFashionRouteHome = new ZRouteBuilder()
+  .name('Fashion')
+  .path('/')
+  .description('Make it look good')
+  .avatar('/images/svg/fashion.svg')
+  .build();
+
+export const ZFashionRouteVenue = new ZRouteBuilder()
+  .name('Venue')
+  .path('venue')
+  .description('What are the building blocks?')
+  .avatar('/images/svg/venue.svg')
+  .build();
+
+export const ZFashionRouteVenueBoolean = new ZRouteBuilder()
+  .name('Boolean')
+  .path('boolean')
+  .description('Basic Togglers')
+  .build();
+
+export const ZFashionRouteVenueButton = new ZRouteBuilder()
+  .name('Button')
+  .path('button')
+  .description('Click To Activate')
+  .build();
+
+export const ZFashionRouteVenueTypography = new ZRouteBuilder()
+  .name('Typography')
+  .path('typography')
+  .description('Standard Page Structures')
+  .build();
+
+export const ZFashionRouteVenueAllComponents = [
+  ZFashionRouteVenueBoolean,
+  ZFashionRouteVenueButton,
+  ZFashionRouteVenueTypography
+];
