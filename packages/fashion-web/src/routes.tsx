d051512d8ce02a6745259921d673abf196c4c4e1
Anthony Bonta <zthun.coding@gmail.com>
2023-02-27 21:18:31 -0600

    refactor: icons

--- packages/fashion-web/src/routes.tsx
+++ packages/fashion-web/src/routes.tsx
@@ -18,18 +18,21 @@ export const ZFashionRouteVenueBoolean = new ZRouteBuilder()
   .name('Boolean')
   .path('boolean')
   .description('Basic Togglers')
+  .avatar('/images/svg/boolean.svg')
   .build();
 
 export const ZFashionRouteVenueButton = new ZRouteBuilder()
   .name('Button')
   .path('button')
   .description('Click To Activate')
+  .avatar('/images/svg/button.svg')
   .build();
 
 export const ZFashionRouteVenueTypography = new ZRouteBuilder()
   .name('Typography')
   .path('typography')
   .description('Standard Page Structures')
+  .avatar('/images/svg/typography.svg')
   .build();
 
 export const ZFashionRouteVenueAllComponents = [
