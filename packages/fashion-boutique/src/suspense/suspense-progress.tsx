1eef95cebc0342a02ca3ece44c12fe87f1efd6ac
Anthony Bonta <zthun.coding@gmail.com>
2023-03-28 00:08:24 -0500

    feat: suspense progress uses animated progress bars

---
+++ packages/fashion-boutique/src/suspense/suspense-progress.tsx
@@ -0,0 +1,51 @@
+import { LinearProgress } from '@mui/material';
+import {
+  createSizeChartFixedArithmetic,
+  createSizeChartFixedCss,
+  ZSizeFixed,
+  ZSizeVaried
+} from '@zthun/fashion-tailor';
+import { cssJoinDefined } from '@zthun/helpful-fn';
+import React from 'react';
+import { createStyleHook } from '../theme/styled';
+import { IZSuspense } from './suspense';
+
+const SuspenseProgressSizeChart = createSizeChartFixedCss(createSizeChartFixedArithmetic(0.25, 0.25), 'rem');
+
+const useSuspenseProgressStyles = createStyleHook((_, props: IZSuspense<ZSizeVaried.Full, ZSizeFixed>) => {
+  const { height = ZSizeFixed.ExtraSmall } = props;
+  const _height = SuspenseProgressSizeChart[height];
+
+  return {
+    root: {
+      height: _height
+    }
+  };
+});
+
+/**
+ * Renders a circular progress that can render nothing or the material ui circular progress.
+ *
+ * @param props -
+ *        The properties for the card.
+ *
+ * @returns The jsx for a circular loading progress.
+ */
+export function ZSuspenseProgress(props: IZSuspense<ZSizeVaried.Full, ZSizeFixed>) {
+  const { className, loading = true, height = ZSizeFixed.ExtraSmall, name } = props;
+  const { classes } = useSuspenseProgressStyles(props);
+
+  if (!loading) {
+    return null;
+  }
+
+  return (
+    <LinearProgress
+      className={cssJoinDefined('ZSuspense-root', 'ZSuspense-progress', className, classes.root)}
+      color='inherit'
+      data-name={name}
+      data-width={ZSizeVaried.Full}
+      data-height={height}
+    />
+  );
+}
