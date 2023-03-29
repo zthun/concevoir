feaa3cdb38e93f948085822d45b39dfa6656db8d
Anthony Bonta <zthun.coding@gmail.com>
2023-03-29 18:52:28 -0500

    test: e2e tests for typography

--- packages/fashion-web/features/typography/typography.feature
+++ packages/fashion-web/features/typography/typography.feature
@@ -1,6 +1,20 @@
 @TypographyPage
 Feature: Typography Page
 
-Scenario: The typography page opens when navigated to
-  When I navigate to the typography demo page
-  Then the page loads successfully
+Background: 
+  Given I navigate to the typography demo page
+
+@TypographyPage-Fashion
+Scenario: Should select the correct fashion
+  When I select the "<fashion>" fashion option on the typography page
+  Then the typography fashion should be "<fashion>" on the typography page
+  Examples:
+    |fashion|
+    |Primary|
+    |Secondary|
+    |Success|
+    |Warning|
+    |Error|
+    |Info|
+    |Light|
+    |Dark|
--- packages/fashion-web/features/typography/typography.ts
+++ packages/fashion-web/features/typography/typography.ts
@@ -1,4 +1,5 @@
-import { When } from '@cucumber/cucumber';
+import { Then, When } from '@cucumber/cucumber';
+import assert from 'assert';
 import { ZTypographyPageComponentModel } from '../../src/boutique/typography/typography-page.cm';
 import { ZFashionRouteBoutique, ZFashionRouteTypography } from '../../src/routes';
 import { ZFashionWorld } from '../fashion-world';
@@ -7,3 +8,20 @@ When('I navigate to the typography demo page', async function (this: ZFashionWor
   await this.open(ZFashionRouteBoutique, ZFashionRouteTypography);
   this.parameters.page = await this.create(ZTypographyPageComponentModel);
 });
+
+When(
+  'I select the {string} fashion option on the typography page',
+  async function (this: ZFashionWorld<ZTypographyPageComponentModel>, name: string) {
+    const fashion = await this.parameters.page.fashion();
+    await fashion.select(name);
+  }
+);
+
+Then(
+  'the typography fashion should be {string} on the typography page',
+  async function (this: ZFashionWorld<ZTypographyPageComponentModel>, name: string) {
+    const color = await this.parameters.page.color();
+    const actual = await color.fashion();
+    assert.equal(name, actual);
+  }
+);
