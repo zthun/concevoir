712a99d3324c4db5053339b8816794809dce9b9a
Anthony Bonta <zthun.coding@gmail.com>
2023-04-01 15:36:09 -0500

    refactor: theme page displays theme colors

---
+++ packages/fashion-web/src/theme/color/fashion-colors.tsx
@@ -0,0 +1,75 @@
+import {
+  IZComponentFashion,
+  ZBox,
+  ZCaption,
+  ZLabel,
+  ZStack,
+  createStyleHook,
+  useFashionTheme
+} from '@zthun/fashion-boutique';
+import { ZSizeFixed } from '@zthun/fashion-tailor';
+import { cssJoinDefined, firstDefined } from '@zthun/helpful-fn';
+import React from 'react';
+
+export interface IZFashionColors extends Required<IZComponentFashion> {}
+
+const useFashionColorsStyles = createStyleHook(({ theme, tailor }, props: IZFashionColors) => {
+  const { fashion } = props;
+  return {
+    block: {
+      height: '4rem',
+      width: '4rem',
+      border: `${tailor.thickness()} solid ${theme.dark.dark}`
+    },
+
+    light: {
+      backgroundColor: firstDefined(fashion.main, fashion.light)
+    },
+
+    main: {
+      backgroundColor: fashion.main
+    },
+
+    dark: {
+      backgroundColor: firstDefined(fashion.main, fashion.dark)
+    },
+
+    contrast: {
+      backgroundColor: fashion.contrast
+    }
+  };
+});
+
+export function ZFashionColors(props: IZFashionColors) {
+  const { fashion } = props;
+  const theme = useFashionTheme();
+  const { classes } = useFashionColorsStyles(props);
+
+  const renderColor = (name: string, c: string) => (
+    <ZStack orientation='vertical' alignItems='center'>
+      <ZCaption compact>{name}</ZCaption>
+      <div className={cssJoinDefined('ZFashionColors-block', classes.block, c)} />
+    </ZStack>
+  );
+
+  return (
+    <ZBox
+      className={cssJoinDefined('ZFashionColors-root')}
+      fashion={theme.body}
+      border={{ width: ZSizeFixed.Small }}
+      padding={ZSizeFixed.Small}
+      data-name={fashion.name}
+      data-fashion={fashion.name}
+    >
+      <ZStack orientation='vertical' gap={ZSizeFixed.Small}>
+        <ZLabel>{fashion.name}</ZLabel>
+        <ZStack orientation='horizontal' gap={ZSizeFixed.Medium}>
+          {renderColor('Light', classes.light)}
+          {renderColor('Main', classes.main)}
+          {renderColor('Dark', classes.dark)}
+          {renderColor('Contrast', classes.contrast)}
+        </ZStack>
+      </ZStack>
+    </ZBox>
+  );
+}
--- packages/fashion-web/src/theme/theme-page.tsx
+++ packages/fashion-web/src/theme/theme-page.tsx
@@ -1,5 +1,68 @@
-import React from 'react';
+import { ZBox, ZCard, ZChoiceDropDown, ZGrid, ZH3, ZImageSource, ZParagraph } from '@zthun/fashion-boutique';
+import { ZSizeFixed } from '@zthun/fashion-tailor';
+import { ZFashionThemeBuilder } from '@zthun/fashion-theme';
+import { setFirst } from '@zthun/helpful-fn';
+import { identity } from 'lodash';
+import React, { useState } from 'react';
+import { ZFashionRouteTheme } from '../routes';
+import { ZFashionColors } from './color/fashion-colors';
 
+const themeDefault = new ZFashionThemeBuilder().build();
+
+/**
+ * Represents the theme page.
+ *
+ * @returns The JSX to render the theme page.
+ */
 export function ZThemePage() {
-  return <div className='ZFashionThemePage-root'>Theme Page</div>;
+  const [theme, setTheme] = useState(themeDefault);
+  const themes = [themeDefault];
+  const _setTheme = setFirst.bind(null, setTheme, themeDefault);
+
+  return (
+    <ZCard
+      className='ZThemePage-root'
+      heading={ZFashionRouteTheme.name}
+      subHeading={ZFashionRouteTheme.description}
+      avatar={<ZImageSource src={ZFashionRouteTheme.avatar} height={ZSizeFixed.Medium} />}
+    >
+      <ZBox margin={{ bottom: ZSizeFixed.Large }}>
+        <ZH3>Description</ZH3>
+
+        <ZParagraph>
+          A fashion theme describes the different makeup of page colors that are used across common places in a website.
+        </ZParagraph>
+
+        <ZBox margin={{ bottom: ZSizeFixed.Medium }}>
+          <ZGrid columns='1fr 1fr 1fr' columnsLg='1fr 1fr' columnsMd='1fr' gap={ZSizeFixed.Medium}>
+            <ZFashionColors fashion={theme.primary} />
+            <ZFashionColors fashion={theme.secondary} />
+            <ZFashionColors fashion={theme.success} />
+            <ZFashionColors fashion={theme.warning} />
+            <ZFashionColors fashion={theme.error} />
+            <ZFashionColors fashion={theme.info} />
+            <ZFashionColors fashion={theme.light} />
+            <ZFashionColors fashion={theme.dark} />
+            <ZFashionColors fashion={theme.body} />
+            <ZFashionColors fashion={theme.surface} />
+          </ZGrid>
+        </ZBox>
+      </ZBox>
+
+      <ZBox margin={{ bottom: ZSizeFixed.Large }}>
+        <ZH3>Options</ZH3>
+
+        <ZGrid gap={ZSizeFixed.Medium}>
+          <ZChoiceDropDown
+            indelible
+            options={themes}
+            value={[theme]}
+            onValueChange={_setTheme}
+            identifier={identity}
+            renderOption={(t) => t.name}
+          />
+        </ZGrid>
+      </ZBox>
+    </ZCard>
+  );
 }
