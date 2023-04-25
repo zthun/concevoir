265f721809802a00c984a44d5ad5ed246bad1397
Anthony Bonta <zthun.coding@gmail.com>
2023-04-25 17:19:06 -0500

    chore: cleanup

--- packages/fashion-boutique/src/label/labeled.tsx
+++ packages/fashion-boutique/src/label/labeled.tsx
@@ -5,16 +5,7 @@ import { IZComponentLabel } from '../component/component-label';
 import { IZComponentName } from '../component/component-name';
 import { IZComponentStyle } from '../component/component-style';
 import { ZStack } from '../stack/stack';
-import { createStyleHook } from '../theme/styled';
 import { ZLabel } from './label';
-
-const useLabeledStyles = createStyleHook(() => ({
-  root: {},
-  label: {
-    display: 'block',
-    fontWeight: 'bold'
-  }
-}));
 
 export interface ZLabeled extends IZComponentLabel, IZComponentStyle, IZComponentName {
   gap?: ZSizeVoid | ZSizeFixed;
@@ -24,11 +15,10 @@ export interface ZLabeled extends IZComponentLabel, IZComponentStyle, IZComponen
 export function ZLabeled(props: ZLabeled) {
   const { children, label, className, name, gap = ZSizeFixed.ExtraSmall } = props;
   const id = useMemo(() => createGuid(), []);
-  const { classes } = useLabeledStyles();
 
   return (
     <ZStack
-      className={cssJoinDefined('ZLabeled-root', className, classes.root)}
+      className={cssJoinDefined('ZLabeled-root', className)}
       orientation={ZOrientation.Vertical}
       gap={gap}
       name={name}
