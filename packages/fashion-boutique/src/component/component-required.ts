36d0d0838ff63e4d4f6251de372ab52dc48f3ccf
Anthony Bonta <zthun.coding@gmail.com>
2023-04-29 09:25:55 -0500

    feat: components can require interaction to move forward

---
+++ packages/fashion-boutique/src/component/component-required.ts
@@ -0,0 +1,9 @@
+/**
+ * Represents a component that has a required property.
+ */
+export interface IZComponentRequired {
+  /**
+   * Specifies that interaction with the target element is required before moving forward.
+   */
+  required?: boolean;
+}
