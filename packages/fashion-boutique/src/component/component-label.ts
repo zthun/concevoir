4154d9ef37f1eb682b214b4b6cc70147d9c44b10
Anthony Bonta <zthun.coding@gmail.com>
2023-05-13 17:06:57 -0500

    docs: tsdoc

--- packages/fashion-boutique/src/component/component-label.ts
+++ packages/fashion-boutique/src/component/component-label.ts
@@ -1,5 +1,11 @@
 import { ReactNode } from 'react';
 
+/**
+ * Represents a component that has a built in label.
+ */
 export interface IZComponentLabel {
+  /**
+   * The built in label node.
+   */
   label?: ReactNode;
 }
