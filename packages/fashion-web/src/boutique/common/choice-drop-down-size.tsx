d933c047974b14289b5a0d99da248a0e987c1148
Anthony Bonta <zthun.coding@gmail.com>
2023-06-14 22:25:56 -0500

    feat: table page allows for table options

---
+++ packages/fashion-web/src/boutique/common/choice-drop-down-size.tsx
@@ -0,0 +1,43 @@
+import { IZComponentName, IZComponentValue, ZChoiceDropDown } from '@zthun/fashion-boutique';
+import { ZSize, ZSizeFixed, ZSizeVaried, ZSizeVoid } from '@zthun/fashion-tailor';
+import { setFirst } from '@zthun/helpful-fn';
+import { useAmbassadorState } from '@zthun/helpful-react';
+import { identity, startCase } from 'lodash';
+import React, { useMemo } from 'react';
+
+export const ZFixedSizes = Object.values(ZSizeFixed);
+export const ZVariedSizes = Object.values(ZSizeVaried);
+export const ZVoidSizes = Object.values(ZSizeVoid);
+
+export interface IZChoiceDropDownSize<TSize> extends IZComponentValue<TSize>, IZComponentName {
+  label: 'Width' | 'Height';
+  sizes: TSize[];
+}
+
+/**
+ * A drop down that allows the user to select a fashion from the theme.
+ *
+ * @param props -
+ *        The properties for this component.
+ *
+ * @returns
+ *        The jsx to render the component.
+ */
+export function ZChoiceDropDownSize<TSize = ZSize>(props: IZChoiceDropDownSize<TSize>) {
+  const { value, onValueChange, name, sizes, label } = props;
+  const [_value, _setValue] = useAmbassadorState(value, onValueChange);
+  const size = useMemo(() => (_value ? [_value] : undefined), [_value]);
+  const _setSize = setFirst.bind(null, _setValue, undefined);
+
+  return (
+    <ZChoiceDropDown
+      label={label}
+      value={size}
+      onValueChange={_setSize}
+      options={sizes}
+      renderOption={(s) => startCase(String(s))}
+      identifier={identity}
+      name={name}
+    />
+  );
+}
--- packages/fashion-web/src/boutique/table/table-page.tsx
+++ packages/fashion-web/src/boutique/table/table-page.tsx
@@ -1,8 +1,9 @@
-import { ZCard, ZImageSource, ZTable } from '@zthun/fashion-boutique';
+import { ZBox, ZCard, ZH3, ZImageSource, ZParagraph, ZTable } from '@zthun/fashion-boutique';
 import { ZSizeFixed } from '@zthun/fashion-tailor';
 import { IZPerson, ZDataSourcePeople, ZPersonBuilder } from '@zthun/helpful-people';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { ZFashionRouteTable } from '../../routes';
+import { ZChoiceDropDownSize, ZFixedSizes } from '../common/choice-drop-down-size';
 
 /**
  * Represents a demo for grid views.
@@ -12,6 +13,7 @@ import { ZFashionRouteTable } from '../../routes';
 export function ZTablePage() {
   const dataSource = useMemo(() => new ZDataSourcePeople(), []);
   const columns = useMemo(() => ZPersonBuilder.metadata(), []);
+  const [height, setHeight] = useState<ZSizeFixed | undefined>(ZSizeFixed.Medium);
 
   return (
     <ZCard
@@ -20,12 +22,27 @@ export function ZTablePage() {
       subHeading={ZFashionRouteTable.description}
       avatar={<ZImageSource src={ZFashionRouteTable.avatar} height={ZSizeFixed.Medium} />}
     >
-      <ZTable
-        className='ZTablePage-table'
-        dataSource={dataSource}
-        columns={columns}
-        identifier={(r: IZPerson) => r.id}
-      />
+      <ZH3>Description</ZH3>
+
+      <ZParagraph>
+        Tabular data can be a great way to show organized data to a user that is used to spreadsheets. The main
+        advantage to tables is that they can be very familiar to most. Do be careful about using them as it may be
+        better to use a grid view instead. Tables tend to not be mobile friendly and they usually do not render well on
+        small devices.
+      </ZParagraph>
+
+      <ZBox margin={{ top: ZSizeFixed.Large, bottom: ZSizeFixed.ExtraLarge }}>
+        <ZTable
+          className='ZTablePage-table'
+          dataSource={dataSource}
+          columns={columns}
+          height={height}
+          identifier={(r: IZPerson) => r.id}
+        />
+      </ZBox>
+
+      <ZH3>Options</ZH3>
+      <ZChoiceDropDownSize label='Height' sizes={ZFixedSizes} value={height} onValueChange={setHeight} />
     </ZCard>
   );
 }
