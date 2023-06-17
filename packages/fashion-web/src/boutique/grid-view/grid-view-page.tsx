5e8d6e7762f63aa6cf8f3210772211aeae61bead
Anthony Bonta <zthun.coding@gmail.com>
2023-06-16 21:43:30 -0500

    feat: grid view demo now shows in a card

--- packages/fashion-web/src/boutique/grid-view/grid-view-page.tsx
+++ packages/fashion-web/src/boutique/grid-view/grid-view-page.tsx
@@ -1,9 +1,19 @@
-import { ZCard, ZGridView, ZIconFontAwesome, ZStack } from '@zthun/fashion-boutique';
+import {
+  ZBox,
+  ZCard,
+  ZGridView,
+  ZH3,
+  ZIconFontAwesome,
+  ZImageSource,
+  ZParagraph,
+  ZStack
+} from '@zthun/fashion-boutique';
 import { ZSizeFixed } from '@zthun/fashion-tailor';
 import { IZBrand, ZBrands } from '@zthun/helpful-brands';
 import { ZOrientation } from '@zthun/helpful-fn';
 import { ZDataSearchFields, ZDataSourceStatic, ZDataSourceStaticOptionsBuilder } from '@zthun/helpful-query';
 import React from 'react';
+import { ZFashionRouteGridView } from 'src/routes';
 
 const ZBrandDataSourceOptions = new ZDataSourceStaticOptionsBuilder()
   .search(new ZDataSearchFields(['id', 'name']))
@@ -26,17 +36,32 @@ export function ZGridViewPage() {
   );
 
   return (
-    <ZGridView
+    <ZCard
       className='ZGridViewPage-root'
-      GridProps={{
-        gap: ZSizeFixed.Small,
-        columns: '1fr 1fr 1fr 1fr',
-        columnsLg: '1fr 1fr 1fr',
-        columnsMd: '1fr 1fr',
-        columnsSm: '1fr'
-      }}
-      renderItem={renderItem}
-      dataSource={ZBrandDataSource}
-    />
+      heading={ZFashionRouteGridView.name}
+      subHeading={ZFashionRouteGridView.description}
+      avatar={<ZImageSource src={ZFashionRouteGridView.avatar} height={ZSizeFixed.Medium} />}
+    >
+      <ZBox margin={{ bottom: ZSizeFixed.Large }}>
+        <ZH3>Description</ZH3>
+
+        <ZParagraph>
+          You will eventually need to deal with large amounts of list data. The recommended way to display this data is
+          with a grid view in the fashion system. Other options can include tables, but it is better to use something
+          that is more mobile friendly in general and grid views tend to be very responsive to this effort.
+        </ZParagraph>
+      </ZBox>
+      <ZGridView
+        GridProps={{
+          gap: ZSizeFixed.Small,
+          columns: '1fr 1fr 1fr 1fr',
+          columnsLg: '1fr 1fr 1fr',
+          columnsMd: '1fr 1fr',
+          columnsSm: '1fr'
+        }}
+        renderItem={renderItem}
+        dataSource={ZBrandDataSource}
+      />
+    </ZCard>
   );
 }
