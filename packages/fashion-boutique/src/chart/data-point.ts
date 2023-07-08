7772c4c1e96d414e816b9dfc32993ef48b778bb8
Anthony Bonta <zthun.coding@gmail.com>
2023-07-08 16:14:57 -0500

    feat: charts allow for easy data visualizations

---
+++ packages/fashion-boutique/src/chart/data-point.ts
@@ -0,0 +1,30 @@
+import { IZFashion } from '@zthun/fashion-theme';
+
+export interface IZDataPoint {
+  x: number;
+  y: number;
+  name?: string;
+  fashion?: IZFashion;
+}
+
+export class ZDataPointBuilder {
+  private _point: IZDataPoint;
+
+  public constructor(x: number, y: number) {
+    this._point = { x, y };
+  }
+
+  public name(val: string): this {
+    this._point.name = val;
+    return this;
+  }
+
+  public fashion(val: IZFashion): this {
+    this._point.fashion = val;
+    return this;
+  }
+
+  public build(): IZDataPoint {
+    return structuredClone(this._point);
+  }
+}
