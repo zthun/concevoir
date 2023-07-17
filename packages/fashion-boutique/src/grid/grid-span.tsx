1a658e43c0e0b60a7768b34ac7a26df784da4d1d
Anthony Bonta <zthun.coding@gmail.com>
2023-07-16 20:30:37 -0500

    fix: grid span now properly reacts to sizing

--- packages/fashion-boutique/src/grid/grid-span.tsx
+++ packages/fashion-boutique/src/grid/grid-span.tsx
@@ -1,3 +1,4 @@
+import { ZSizeFixed } from '@zthun/fashion-tailor';
 import { cssJoinDefined } from '@zthun/helpful-fn';
 import { Property } from 'csstype';
 import React from 'react';
@@ -7,20 +8,91 @@ import { createStyleHook } from '../theme/styled';
 
 export interface IZGridSpan extends IZComponentHierarchy, IZComponentStyle {
   columnStart?: Property.GridColumnStart;
+  columnStartLg?: Property.GridColumnStart;
+  columnStartMd?: Property.GridColumnStart;
+  columnStartSm?: Property.GridColumnStart;
+  columnStartXs?: Property.GridColumnStart;
+
   columnEnd?: Property.GridColumnEnd;
+  columnEndLg?: Property.GridColumnEnd;
+  columnEndMd?: Property.GridColumnEnd;
+  columnEndSm?: Property.GridColumnEnd;
+  columnEndXs?: Property.GridColumnEnd;
+
   rowStart?: Property.GridRowStart;
+  rowStartLg?: Property.GridRowStart;
+  rowStartMd?: Property.GridRowStart;
+  rowStartSm?: Property.GridRowStart;
+  rowStartXs?: Property.GridRowStart;
+
   rowEnd?: Property.GridRowEnd;
+  rowEndLg?: Property.GridRowEnd;
+  rowEndMd?: Property.GridRowEnd;
+  rowEndSm?: Property.GridRowEnd;
+  rowEndXs?: Property.GridRowEnd;
 }
 
-const useGridSpanStyles = createStyleHook((_, props: IZGridSpan) => {
-  const { columnStart, columnEnd, rowStart, rowEnd } = props;
+const useGridSpanStyles = createStyleHook(({ device }, props: IZGridSpan) => {
+  const {
+    columnStart,
+    columnStartLg = columnStart,
+    columnStartMd = columnStartLg,
+    columnStartSm = columnStartMd,
+    columnStartXs = columnStartSm,
+
+    columnEnd,
+    columnEndLg = columnEnd,
+    columnEndMd = columnEndLg,
+    columnEndSm = columnEndMd,
+    columnEndXs = columnEndSm,
+
+    rowStart,
+    rowStartLg = rowStart,
+    rowStartMd = rowStartLg,
+    rowStartSm = rowStartMd,
+    rowStartXs = rowStartSm,
+
+    rowEnd,
+    rowEndLg = rowEnd,
+    rowEndMd = rowEndLg,
+    rowEndSm = rowEndMd,
+    rowEndXs = rowEndSm
+  } = props;
 
   return {
     root: {
       gridColumnStart: columnStart,
       gridColumnEnd: columnEnd,
       gridRowStart: rowStart,
-      gridRowEnd: rowEnd
+      gridRowEnd: rowEnd,
+
+      [device.break(ZSizeFixed.Large)]: {
+        gridColumnStart: columnStartLg,
+        gridColumnEnd: columnEndLg,
+        gridRowStart: rowStartLg,
+        gridRowEnd: rowEndLg
+      },
+
+      [device.break(ZSizeFixed.Medium)]: {
+        gridColumnStart: columnStartMd,
+        gridColumnEnd: columnEndMd,
+        gridRowStart: rowStartMd,
+        gridRowEnd: rowEndMd
+      },
+
+      [device.break(ZSizeFixed.Small)]: {
+        gridColumnStart: columnStartSm,
+        gridColumnEnd: columnEndSm,
+        gridRowStart: rowStartSm,
+        gridRowEnd: rowEndSm
+      },
+
+      [device.break(ZSizeFixed.ExtraSmall)]: {
+        gridColumnStart: columnStartXs,
+        gridColumnEnd: columnEndXs,
+        gridRowStart: rowStartXs,
+        gridRowEnd: rowEndXs
+      }
     }
   };
 });
