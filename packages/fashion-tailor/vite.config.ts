0128cc661d3017c4ca74f6a7fc16696023562aa9
Anthony Bonta <zthun.coding@gmail.com>
2023-12-15 20:04:03 -0600

    build: move vite to root

--- packages/fashion-tailor/vite.config.ts
+++ packages/fashion-tailor/vite.config.ts
@@ -1,16 +1,2 @@
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
-import { externalizeDeps } from 'vite-plugin-externalize-deps';
-
-export default defineConfig({
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      formats: ['es', 'cjs'],
-      fileName: 'index'
-    },
-    minify: false,
-    sourcemap: true
-  },
-  plugins: [externalizeDeps(), dts({ tsconfigPath: 'tsconfig.prod.json' })]
-});
+import { defineLibrary } from '../../.config/vite-library';
+export default defineLibrary(__dirname);
