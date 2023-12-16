9fdfa5aa9caa2272f25ae852f8edfd9d12e94bea
Anthony Bonta <zthun.coding@gmail.com>
2023-12-15 20:04:30 -0600

    refactor: move vite to root

--- packages/fashion-web/vite.config.ts
+++ packages/fashion-web/vite.config.ts
@@ -1,20 +1,2 @@
-import { visualizer } from 'rollup-plugin-visualizer';
-import { PluginOption, defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-  plugins: [
-    tsConfigPaths(),
-    visualizer({
-      filename: 'stats/analysis.html'
-    }) as PluginOption
-  ],
-  server: {
-    strictPort: true
-  },
-  resolve: {
-    alias: {
-      lodash: 'lodash-es'
-    }
-  }
-});
+import { defineWeb } from '../../.config/vite-web';
+export default defineWeb(__dirname);
