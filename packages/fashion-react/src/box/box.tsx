463b53b48d3dd663c8066a1c810210c604cc1f04
Anthony Bonta <zthun.coding@gmail.com>
2024-02-16 14:01:13 -0600

    refactor: use box web component under the hood

--- packages/fashion-react/src/box/box.tsx
+++ packages/fashion-react/src/box/box.tsx
@@ -1,25 +1,30 @@
 import {
-  createSizeChartFixedCss,
-  createSizeChartFixedGeometric,
-  createSizeChartVariedCss,
-  createSizeChartVoidCss,
-  ZSizeFixed,
-  ZSizeVaried,
-  ZSizeVoid
-} from '@zthun/fashion-tailor';
-import { IZFashion, transparent, ZColor } from '@zthun/fashion-theme';
-import { cssJoinDefined, firstDefined, ZHorizontalAnchor } from '@zthun/helpful-fn';
+  ZBoxBorderElement,
+  ZBoxElement,
+  ZBoxMarginElement,
+  ZBoxPaddingElement,
+  ZBoxWidthElement
+} from '@zthun/fashion-boutique';
+import { ZSize, ZSizeFixed, ZSizeVaried, ZSizeVoid } from '@zthun/fashion-tailor';
+import { ZHorizontalAnchor, cssJoinDefined, firstDefined } from '@zthun/helpful-fn';
 import { Property } from 'csstype';
 import { get } from 'lodash-es';
-import React, { MouseEventHandler } from 'react';
+import React, { MouseEventHandler, useEffect } from 'react';
 import { IZComponentFashion } from '../component/component-fashion.mjs';
 import { IZComponentHierarchy } from '../component/component-hierarchy.mjs';
 import { IZComponentStyle } from '../component/component-style.mjs';
 import { IZComponentWidth } from '../component/component-width.mjs';
-import { createStyleHook } from '../theme/styled';
 
-interface IZBorderProps extends Pick<IZComponentWidth<ZSizeFixed | ZSizeVoid>, 'width'> {
-  style?: Property.BorderStyle;
+declare global {
+  namespace React.JSX {
+    interface IntrinsicElements {
+      ['z-box']: ZBoxElement & any;
+      ['z-box-border']: ZBoxBorderElement & any;
+      ['z-box-padding']: ZBoxPaddingElement & any;
+      ['z-box-margin']: ZBoxMarginElement & any;
+      ['z-box-width']: ZBoxWidthElement & any;
+    }
+  }
 }
 
 type ZDimensionProps<TSizes> =
@@ -27,17 +32,12 @@ type ZDimensionProps<TSizes> =
   | { x?: TSizes; y?: TSizes }
   | { left?: TSizes; right?: TSizes; top?: TSizes; bottom?: TSizes };
 
-const BoxSizeChart = {
-  ...createSizeChartFixedCss(createSizeChartFixedGeometric(1.4, 18), 'rem'),
-  ...createSizeChartVariedCss(),
-  ...createSizeChartVoidCss()
-};
+interface IZBorderProps extends Pick<IZComponentWidth<ZDimensionProps<ZSizeFixed | ZSizeVoid>>, 'width'> {
+  style?: Property.BorderStyle;
+}
 
 export interface IZBox extends IZComponentHierarchy, IZComponentStyle, IZComponentWidth, IZComponentFashion {
   border?: IZBorderProps;
-
-  focus?: IZFashion;
-  hover?: IZFashion;
 
   padding?: ZDimensionProps<ZSizeFixed | ZSizeVoid>;
   margin?: ZDimensionProps<ZSizeFixed | ZSizeVaried.Fit | ZSizeVoid>;
@@ -46,118 +46,6 @@ export interface IZBox extends IZComponentHierarchy, IZComponentStyle, IZCompone
 
   onClick?: MouseEventHandler;
 }
-
-const useBoxStyles = createStyleHook(({ theme, tailor, device }, props: IZBox) => {
-  const {
-    padding,
-    margin,
-    border,
-    width = ZSizeVaried.Fit,
-    widthLg = width,
-    widthMd = widthLg,
-    widthSm = widthMd,
-    widthXs = widthSm,
-    fashion,
-    focus,
-    hover,
-    justification,
-    onClick
-  } = props;
-
-  const _border = {
-    width: firstDefined(ZSizeVoid.None, border?.width),
-    style: firstDefined('solid', border?.style),
-    fashion: firstDefined(transparent(), fashion?.border),
-    focus: firstDefined(transparent(), focus?.border, fashion?.border),
-    hover: firstDefined(transparent(), hover?.border, fashion?.border)
-  };
-
-  const _background = {
-    fashion: firstDefined(theme.transparent, fashion),
-    focus: firstDefined(theme.transparent, focus, fashion),
-    hover: firstDefined(theme.transparent, hover, fashion)
-  };
-
-  const __border = (color: ZColor) => `${tailor.thickness(_border.width)} ${_border.style} ${color}`;
-
-  const asPadding = (pad: ZSizeFixed | ZSizeVoid | object) => {
-    const size = typeof pad === 'object' ? ZSizeVoid.None : pad;
-    return tailor.gap(size);
-  };
-
-  const asMargin = (margin: ZSizeFixed | ZSizeVoid | ZSizeVaried.Fit | object) => {
-    if (typeof margin === 'object') {
-      return tailor.gap(ZSizeVoid.None);
-    }
-
-    if (margin === ZSizeVaried.Fit) {
-      return 'auto';
-    }
-
-    return tailor.gap(margin);
-  };
-
-  const dimensions = {
-    maxWidth: BoxSizeChart[width],
-
-    [device.break(ZSizeFixed.Large)]: {
-      maxWidth: BoxSizeChart[widthLg]
-    },
-
-    [device.break(ZSizeFixed.Medium)]: {
-      maxWidth: BoxSizeChart[widthMd]
-    },
-
-    [device.break(ZSizeFixed.Small)]: {
-      maxWidth: BoxSizeChart[widthSm]
-    },
-
-    [device.break(ZSizeFixed.ExtraSmall)]: {
-      maxWidth: BoxSizeChart[widthXs]
-    }
-  };
-
-  const pLeft = firstDefined(ZSizeVoid.None, get(padding, 'left'), get(padding, 'x'), padding);
-  const pRight = firstDefined(ZSizeVoid.None, get(padding, 'right'), get(padding, 'x'), padding);
-  const pTop = firstDefined(ZSizeVoid.None, get(padding, 'top'), get(padding, 'y'), padding);
-  const pBottom = firstDefined(ZSizeVoid.None, get(padding, 'bottom'), get(padding, 'y'), padding);
-
-  const mLeft = firstDefined(ZSizeVoid.None, get(margin, 'left'), get(margin, 'x'), margin);
-  const mRight = firstDefined(ZSizeVoid.None, get(margin, 'right'), get(margin, 'x'), margin);
-  const mTop = firstDefined(ZSizeVoid.None, get(margin, 'top'), get(margin, 'y'), margin);
-  const mBottom = firstDefined(ZSizeVoid.None, get(margin, 'bottom'), get(margin, 'y'), margin);
-
-  return {
-    root: {
-      ...dimensions,
-      'cursor': onClick ? 'pointer' : 'default',
-      'border': __border(_border.fashion),
-      'backgroundColor': _background.fashion.main,
-      'color': _background.fashion.contrast,
-
-      '&:focus': {
-        border: __border(_border.focus),
-        backgroundColor: _background.focus.main,
-        color: _background.focus.contrast
-      },
-
-      '&:hover': {
-        border: __border(_border.hover),
-        backgroundColor: _background.hover.main,
-        color: _background.hover.contrast
-      },
-      'paddingLeft': asPadding(pLeft),
-      'paddingRight': asPadding(pRight),
-      'paddingTop': asPadding(pTop),
-      'paddingBottom': asPadding(pBottom),
-      'marginLeft': asMargin(mLeft),
-      'marginRight': asMargin(mRight),
-      'marginTop': asMargin(mTop),
-      'marginBottom': asMargin(mBottom),
-      'textAlign': justification
-    }
-  };
-});
 
 /**
  * Just a box.
@@ -169,13 +57,56 @@ const useBoxStyles = createStyleHook(({ theme, tailor, device }, props: IZBox) =
  *        The JSX to render the box.
  */
 export function ZBox(props: IZBox) {
-  const { className, children, onClick } = props;
-  const { classes } = useBoxStyles(props);
+  const { className, fashion, justification, onClick } = props;
+  const { width, widthLg, widthMd, widthSm, widthXs } = props;
+  const { margin } = props;
+  const { border } = props;
+  const { padding } = props;
+  const { children } = props;
   const tabIndex = onClick ? 0 : undefined;
 
+  const asDimension = (d: ZSize | object) => (typeof d === 'object' ? ZSizeVoid.None : d);
+
+  const pl = asDimension(firstDefined(ZSizeVoid.None, get(padding, 'left'), get(padding, 'x'), padding));
+  const pr = asDimension(firstDefined(ZSizeVoid.None, get(padding, 'right'), get(padding, 'x'), padding));
+  const pt = asDimension(firstDefined(ZSizeVoid.None, get(padding, 'top'), get(padding, 'y'), padding));
+  const pb = asDimension(firstDefined(ZSizeVoid.None, get(padding, 'bottom'), get(padding, 'y'), padding));
+
+  const ml = asDimension(firstDefined(ZSizeVoid.None, get(margin, 'left'), get(margin, 'x'), margin));
+  const mr = asDimension(firstDefined(ZSizeVoid.None, get(margin, 'right'), get(margin, 'x'), margin));
+  const mt = asDimension(firstDefined(ZSizeVoid.None, get(margin, 'top'), get(margin, 'y'), margin));
+  const mb = asDimension(firstDefined(ZSizeVoid.None, get(margin, 'bottom'), get(margin, 'y'), margin));
+
+  const bw = border?.width;
+  const bl = asDimension(firstDefined(ZSizeVoid.None, get(bw, 'left'), get(bw, 'x'), bw));
+  const br = asDimension(firstDefined(ZSizeVoid.None, get(bw, 'right'), get(bw, 'x'), bw));
+  const bt = asDimension(firstDefined(ZSizeVoid.None, get(bw, 'top'), get(bw, 'y'), bw));
+  const bb = asDimension(firstDefined(ZSizeVoid.None, get(bw, 'bottom'), get(bw, 'y'), bw));
+  const bk = border?.style;
+
+  useEffect(() => ZBoxElement.register(), []);
+  useEffect(() => ZBoxBorderElement.register(), []);
+  useEffect(() => ZBoxWidthElement.register(), []);
+  useEffect(() => ZBoxPaddingElement.register(), []);
+  useEffect(() => ZBoxMarginElement.register(), []);
+
   return (
-    <div className={cssJoinDefined('ZBox-root', className, classes.root)} tabIndex={tabIndex} onClick={onClick}>
-      {children}
-    </div>
+    <z-box
+      class={cssJoinDefined(className)}
+      fashion={fashion?.name}
+      tabIndex={tabIndex}
+      justification={justification}
+      onClick={onClick}
+    >
+      <z-box-width width={width} width-lg={widthLg} width-md={widthMd} width-sm={widthSm} width-xs={widthXs}>
+        <z-box-margin left={ml} right={mr} top={mt} bottom={mb}>
+          <z-box-border left={bl} right={br} top={bt} bottom={bb} kind={bk}>
+            <z-box-padding left={pl} right={pr} top={pt} bottom={pb}>
+              {children}
+            </z-box-padding>
+          </z-box-border>
+        </z-box-margin>
+      </z-box-width>
+    </z-box>
   );
 }
