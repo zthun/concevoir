26bd1e122b50e414e7035168715639d27b6848cb
Anthony Bonta <zthun.coding@gmail.com>
2024-02-19 12:47:09 -0600

    chore: turn off brace style and let prettier handle it

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
 const options = require('@zthun/lint-janitor-config/eslint');
 options.rules['@typescript-eslint/no-namespace'] = 'off';
+options.rules['brace-style'] = 'off';
 module.exports = options;
