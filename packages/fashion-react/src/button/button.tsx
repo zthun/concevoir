51da1045ce755e563574f147d04ef616e28c47a1
Anthony Bonta <zthun.coding@gmail.com>
2024-03-02 16:46:38 -0600

    refactor!: button tooltip now uses title which only supports strings

--- packages/fashion-react/src/button/button.tsx
+++ packages/fashion-react/src/button/button.tsx
@@ -1,5 +1,4 @@
-import { Tooltip } from '@mui/material';
-import React, { ReactNode, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import {
   IZComponentDisabled,
@@ -8,12 +7,14 @@ import {
   IZComponentWidth,
   ZButtonElement
 } from '@zthun/fashion-boutique';
-import { ZSizeVaried } from '@zthun/fashion-tailor';
+import { ZDeviceBounds, ZSizeVaried } from '@zthun/fashion-tailor';
 import { includeCustomElement } from '@zthun/helpful-dom';
 import { IZComponentAvatar } from '../component/component-avatar.mjs';
 import { IZComponentLabel } from '../component/component-label.mjs';
 import { IZComponentName } from '../component/component-name.mjs';
 import { IZComponentStyle } from '../component/component-style.mjs';
+
+import '../background/device';
 
 declare global {
   namespace React.JSX {
@@ -35,7 +36,7 @@ export interface IZButton
   borderless?: boolean;
   compact?: boolean;
   outline?: boolean;
-  tooltip?: ReactNode;
+  tooltip?: string;
 
   onClick?: (e: React.MouseEvent) => any;
 }
@@ -49,29 +50,42 @@ export interface IZButton
  * @returns The JSX to render this button.
  */
 export function ZButton(props: IZButton) {
-  const { avatar, className, borderless, compact, disabled, fashion, loading, label, name, outline, tooltip, onClick } =
-    props;
+  const {
+    avatar,
+    className,
+    borderless,
+    compact,
+    disabled,
+    fashion,
+    loading,
+    label,
+    name,
+    outline,
+    tooltip,
+    width,
+    onClick
+  } = props;
+
+  const $width = useMemo(() => new ZDeviceBounds(width, ZSizeVaried.Fit).toDeviceMap(), [width]);
 
   useMemo(() => includeCustomElement(ZButtonElement), []);
 
   return (
-    <Tooltip title={tooltip}>
-      <span>
-        <z-button
-          borderless={borderless}
-          class={className}
-          compact={compact}
-          disabled={disabled}
-          fashion={fashion}
-          loading={loading}
-          outline={outline}
-          name={name}
-          onClick={onClick}
-        >
-          <div slot='prefix'>{avatar}</div>
-          <div>{label}</div>
-        </z-button>
-      </span>
-    </Tooltip>
+    <z-button
+      borderless={borderless}
+      class={className}
+      compact={compact}
+      disabled={disabled}
+      fashion={fashion}
+      loading={loading}
+      outline={outline}
+      name={name}
+      title={tooltip}
+      onClick={onClick}
+    >
+      <z-device xl={$width.xl} lg={$width.lg} md={$width.md} sm={$width.sm} xs={$width.xs} name='width'></z-device>
+      <div slot='prefix'>{avatar}</div>
+      <div>{label}</div>
+    </z-button>
   );
 }
