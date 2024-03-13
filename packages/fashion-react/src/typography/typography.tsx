fb3ab19d52e9e347cb6c41174dd14699ebd19370
Anthony Bonta <zthun.coding@gmail.com>
2024-03-12 22:54:57 -0500

    refactor: use web components for paragraphs

--- packages/fashion-react/src/typography/typography.tsx
+++ packages/fashion-react/src/typography/typography.tsx
@@ -1,9 +1,17 @@
 import { Typography } from '@mui/material';
+import {
+  IZComponentFashion,
+  ZParagraphBodyElement,
+  ZParagraphCaptionElement,
+  ZParagraphOverlineElement,
+  ZParagraphSubtitleElement
+} from '@zthun/fashion-boutique';
+import { includeCustomElement } from '@zthun/helpful-dom';
 import React, { ElementType } from 'react';
 import { IZComponentHierarchy } from '../component/component-hierarchy.mjs';
 import { IZComponentStyle } from '../component/component-style.mjs';
 
-export interface IZTypographyProps extends IZComponentHierarchy, IZComponentStyle {
+export interface IZTypographyProps extends IZComponentHierarchy, IZComponentStyle, IZComponentFashion {
   compact?: boolean;
 }
 
@@ -30,6 +38,20 @@ const render = (variant: Variant, component: ElementType, props: IZTypographyPro
   );
 };
 
+const renderParagraph = (is: string, props: IZTypographyProps) => {
+  const { fashion, children, compact } = props;
+  return (
+    <p is={is} data-fashion={fashion} data-compact={compact}>
+      {children}
+    </p>
+  );
+};
+
+includeCustomElement(ZParagraphBodyElement);
+includeCustomElement(ZParagraphCaptionElement);
+includeCustomElement(ZParagraphSubtitleElement);
+includeCustomElement(ZParagraphOverlineElement);
+
 export const ZH1 = (props: IZTypographyProps) => render('h1', 'h1', props);
 export const ZH2 = (props: IZTypographyProps) => render('h2', 'h2', props);
 export const ZH3 = (props: IZTypographyProps) => render('h3', 'h3', props);
@@ -37,7 +59,7 @@ export const ZH4 = (props: IZTypographyProps) => render('h4', 'h4', props);
 export const ZH5 = (props: IZTypographyProps) => render('h5', 'h5', props);
 export const ZH6 = (props: IZTypographyProps) => render('h6', 'h6', props);
 
-export const ZParagraph = (props: IZTypographyProps) => render('body1', 'p', props);
-export const ZSubtitle = (props: IZTypographyProps) => render('subtitle1', 'p', props);
-export const ZCaption = (props: IZTypographyProps) => render('caption', 'p', props);
-export const ZOverline = (props: IZTypographyProps) => render('overline', 'p', props);
+export const ZParagraph = (props: IZTypographyProps) => renderParagraph('z-paragraph-body', props);
+export const ZSubtitle = (props: IZTypographyProps) => renderParagraph('z-paragraph-subtitle', props);
+export const ZCaption = (props: IZTypographyProps) => renderParagraph('z-paragraph-caption', props);
+export const ZOverline = (props: IZTypographyProps) => renderParagraph('z-paragraph-overline', props);
