6b212e999b475c728d287c65e8fd256b53687227
Anthony Bonta <zthun.coding@gmail.com>
2024-03-15 10:26:45 -0500

    feat!: component-name comes from boutique

--- packages/fashion-react/src/index.ts
+++ packages/fashion-react/src/index.ts
@@ -26,7 +26,6 @@ export * from './component/component-fashion.mjs';
 export * from './component/component-heading.mjs';
 export * from './component/component-hierarchy.mjs';
 export * from './component/component-label.mjs';
-export * from './component/component-name.mjs';
 export * from './component/component-range.mjs';
 export * from './component/component-source.mjs';
 export * from './component/component-style.mjs';
