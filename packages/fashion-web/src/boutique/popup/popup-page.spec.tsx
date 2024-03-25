f5d3ae1dc10832ef57620b0b7ba8d95e667062a5
Anthony Bonta <zthun.coding@gmail.com>
2024-03-25 16:13:07 -0500

    test: cleanup

--- packages/fashion-web/src/boutique/popup/popup-page.spec.tsx
+++ packages/fashion-web/src/boutique/popup/popup-page.spec.tsx
@@ -1,16 +1,25 @@
-import { ZCircusBy } from '@zthun/cirque';
+import { IZCircusDriver, IZCircusSetup, ZCircusBy } from '@zthun/cirque';
 import { ZCircusSetupRenderer } from '@zthun/cirque-du-react';
 import React from 'react';
-import { describe, expect, it } from 'vitest';
+import { afterEach, describe, expect, it } from 'vitest';
 import { ZPopupPage } from './popup-page';
 import { ZPopupPageComponentModel } from './popup-page.cm.mjs';
 
 describe('ZPopupPage', () => {
+  let _renderer: IZCircusSetup<IZCircusDriver>;
+  let _driver: IZCircusDriver;
+
   async function createTestTarget() {
     const element = <ZPopupPage />;
-    const driver = await new ZCircusSetupRenderer(element).setup();
-    return ZCircusBy.first(driver, ZPopupPageComponentModel);
+    _renderer = new ZCircusSetupRenderer(element);
+    _driver = await _renderer.setup();
+    return ZCircusBy.first(_driver, ZPopupPageComponentModel);
   }
+
+  afterEach(async () => {
+    await _driver.destroy?.call(_driver);
+    await _renderer.destroy?.call(_renderer);
+  });
 
   it('should open the popup', async () => {
     // Arrange.
