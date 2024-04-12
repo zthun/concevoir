d4aa0cc6da00eab5ef598b34dcc597af0f344884
Anthony Bonta <zthun.coding@gmail.com>
2024-04-12 16:17:40 -0500

    refactor: removed dependency on createStyleHook

--- packages/fashion-web/src/home/home-page.tsx
+++ packages/fashion-web/src/home/home-page.tsx
@@ -1,29 +1,19 @@
 import {
-  createStyleHook,
   useFashionTheme,
   useNavigate,
   ZBox,
   ZButton,
   ZCard,
+  ZGrid,
   ZIconFontAwesome,
-  ZParagraph
+  ZParagraph,
+  ZStack
 } from '@zthun/fashion-react';
-import { ZSizeFixed, ZSizeVaried } from '@zthun/fashion-tailor';
+import { ZGapSize, ZSizeFixed, ZSizeVaried, ZSizeVoid } from '@zthun/fashion-tailor';
 import { ZFashionPriority } from '@zthun/fashion-theme';
+import { ZOrientation, ZQuadrilateralBuilder } from '@zthun/helpful-fn';
 import React from 'react';
 import { ZFashionRouteBoutique, ZFashionRouteTheme } from '../routes.mjs';
-
-const useHomePageStyles = createStyleHook(({ tailor }) => ({
-  section: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginBottom: tailor.gap()
-  },
-
-  avatar: {
-    textAlign: 'center'
-  }
-}));
 
 /**
  * Renders the home page.
@@ -31,7 +21,6 @@ const useHomePageStyles = createStyleHook(({ tailor }) => ({
  * @returns The jsx that renders the home page.
  */
 export function ZHomePage() {
-  const { classes } = useHomePageStyles();
   const navigate = useNavigate();
   const { primary } = useFashionTheme();
 
@@ -46,64 +35,70 @@ export function ZHomePage() {
   );
 
   return (
-    <div className='ZHomePage-root'>
-      <ZCard
-        className={classes.section}
-        width={ZSizeFixed.ExtraLarge}
-        heading={ZFashionRouteTheme.name}
-        subHeading={ZFashionRouteTheme.description}
-        footer={renderGetStarted('theme')}
-      >
-        <ZBox className={classes.avatar} margin={{ bottom: ZSizeFixed.Medium }}>
-          <ZIconFontAwesome
-            name={ZFashionRouteTheme.avatar}
-            width={{ xl: ZSizeFixed.ExtraLarge, xs: ZSizeFixed.Large }}
-            fashion={primary}
-          />
-        </ZBox>
+    <ZGrid className='ZHomePage-root' justify={{ content: 'center' }}>
+      <ZBox margin={new ZQuadrilateralBuilder<ZGapSize>(ZSizeVoid.None).bottom(ZSizeFixed.Medium).build()}>
+        <ZCard
+          width={ZSizeFixed.ExtraLarge}
+          heading={ZFashionRouteTheme.name}
+          subHeading={ZFashionRouteTheme.description}
+          footer={renderGetStarted('theme')}
+        >
+          <ZBox margin={{ bottom: ZSizeFixed.Medium }}>
+            <ZStack orientation={ZOrientation.Horizontal} justifyContent='center'>
+              <ZIconFontAwesome
+                name={ZFashionRouteTheme.avatar}
+                width={{ xl: ZSizeFixed.ExtraLarge, xs: ZSizeFixed.Large }}
+                fashion={primary}
+              />
+            </ZStack>
+          </ZBox>
 
-        <ZParagraph>
-          The Zthunworks fashion system is a set of helper libraries that make it easier to quickly build a small web
-          application. Small web applications are easier to maintain and having a lot of small web applications that all
-          look and feel similar creates a better user experience. One of the best examples of taking a Monolith
-          application and converting it down to smaller pieces was iTunes by Apple. While iTunes was excellent for
-          playing and managing music, the features to manage movies and photos was out of place and caused iTunes to
-          become very bloated with features. Apple eventually split the Monolith into Music, Photos, Podcasts, Books and
-          TV.
-        </ZParagraph>
+          <ZParagraph>
+            The Zthunworks fashion system is a set of helper libraries that make it easier to quickly build a small web
+            application. Small web applications are easier to maintain and having a lot of small web applications that
+            all look and feel similar creates a better user experience. One of the best examples of taking a Monolith
+            application and converting it down to smaller pieces was iTunes by Apple. While iTunes was excellent for
+            playing and managing music, the features to manage movies and photos was out of place and caused iTunes to
+            become very bloated with features. Apple eventually split the Monolith into Music, Photos, Podcasts, Books
+            and TV.
+          </ZParagraph>
 
-        <ZParagraph>
-          Fashion is built around this philosophy; users are most productive when they are focused on a single task at a
-          time.
-        </ZParagraph>
+          <ZParagraph>
+            Fashion is built around this philosophy; users are most productive when they are focused on a single task at
+            a time.
+          </ZParagraph>
 
-        <ZParagraph>
-          At the root of the entire system is the theme. The theme describes the color scheme of your page and
-          simplifies the way you handle the visual aspect of your site.
-        </ZParagraph>
-      </ZCard>
+          <ZParagraph>
+            At the root of the entire system is the theme. The theme describes the color scheme of your page and
+            simplifies the way you handle the visual aspect of your site.
+          </ZParagraph>
+        </ZCard>
+      </ZBox>
 
-      <ZCard
-        className={classes.section}
-        width={ZSizeFixed.ExtraLarge}
-        heading={ZFashionRouteBoutique.name}
-        subHeading={ZFashionRouteBoutique.description}
-        footer={renderGetStarted('boutique')}
-      >
-        <ZBox className={classes.avatar} margin={{ bottom: ZSizeFixed.Medium }}>
-          <ZIconFontAwesome
-            name={ZFashionRouteBoutique.avatar}
-            width={{ xl: ZSizeFixed.ExtraLarge, xs: ZSizeFixed.Large }}
-            fashion={primary}
-          />
-        </ZBox>
+      <ZBox margin={new ZQuadrilateralBuilder<ZGapSize>(ZSizeVoid.None).bottom(ZSizeFixed.Medium).build()}>
+        <ZCard
+          width={ZSizeFixed.ExtraLarge}
+          heading={ZFashionRouteBoutique.name}
+          subHeading={ZFashionRouteBoutique.description}
+          footer={renderGetStarted('boutique')}
+        >
+          <ZBox margin={{ bottom: ZSizeFixed.Medium }}>
+            <ZStack orientation={ZOrientation.Horizontal} justifyContent='center'>
+              <ZIconFontAwesome
+                name={ZFashionRouteBoutique.avatar}
+                width={{ xl: ZSizeFixed.ExtraLarge, xs: ZSizeFixed.Large }}
+                fashion={primary}
+              />
+            </ZStack>
+          </ZBox>
 
-        <ZParagraph>
-          You need materials, tools, and designs to put on a show. The fashion boutique contains react components needed
-          to build a web page. The primary focus of the boutique is to help form a common look and feel across several
-          single page applications, given a single theme.
-        </ZParagraph>
-      </ZCard>
-    </div>
+          <ZParagraph>
+            You need materials, tools, and designs to put on a show. The fashion boutique contains react components
+            needed to build a web page. The primary focus of the boutique is to help form a common look and feel across
+            several single page applications, given a single theme.
+          </ZParagraph>
+        </ZCard>
+      </ZBox>
+    </ZGrid>
   );
 }
