73d1e94c9d5843f945ba590d1ea0956ea18441f7
Anthony Bonta <zthun.coding@gmail.com>
2024-04-12 16:18:34 -0500

    chore: cleanup

--- packages/fashion-react/src/card/card.tsx
+++ packages/fashion-react/src/card/card.tsx
@@ -38,72 +38,6 @@ export interface IZCard
   footer?: ReactNode;
 }
 
-/*
-const useCardStyles = createStyleHook(({ theme, device }, props: IZCard) => {
-  const { surface } = theme;
-  const { width = ZSizeVaried.Fit, height = ZSizeVaried.Fit, fashion = surface } = props;
-
-  const $width = castDeviceMap(width, ZSizeVaried.Fit);
-  const $height = castDeviceMap(height, ZSizeVaried.Fit);
-
-  const maxWidth = {
-    maxWidth: CardWidthChart[$width.xl],
-    minHeight: CardHeightChart[$height.xl],
-
-    [device.break(ZSizeFixed.Large)]: {
-      maxWidth: CardWidthChart[$width.lg],
-      minHeight: CardHeightChart[$height.lg]
-    },
-
-    [device.break(ZSizeFixed.Medium)]: {
-      maxWidth: CardWidthChart[$width.md],
-      minHeight: CardHeightChart[$height.md]
-    },
-
-    [device.break(ZSizeFixed.Small)]: {
-      maxWidth: CardWidthChart[$width.sm],
-      minHeight: CardHeightChart[$height.sm]
-    },
-
-    [device.break(ZSizeFixed.ExtraSmall)]: {
-      maxWidth: CardWidthChart[$width.xs],
-      minHeight: CardHeightChart[$height.xs]
-    }
-  };
-
-  return {
-    root: {
-      ...maxWidth,
-      'position': 'relative',
-      'backgroundColor': fashion.main,
-      'color': fashion.contrast,
-      'display': 'flex',
-      'flexDirection': 'column',
-
-      '.MuiCardHeader-content': {
-        flex: '1 1 0px',
-        overflow: 'hidden'
-      },
-
-      '.MuiCardHeader-subheader': {
-        color: fashion.contrast,
-        opacity: 0.9
-      }
-    },
-
-    content: {
-      flexGrow: 1
-    },
-
-    title: {
-      overflow: 'hidden',
-      whiteSpace: 'nowrap',
-      textOverflow: 'ellipsis'
-    }
-  };
-});
-*/
-
 /**
  * Represents a basic card component.
  *
@@ -119,39 +53,6 @@ export function ZCard(props: IZCard) {
   useWebComponent(ZDeviceElement);
   const $width = new ZDeviceBounds(width, ZSizeVaried.Fit);
   const $height = new ZDeviceBounds(height, ZSizeVaried.Fit);
-
-  /*
-  const renderHeader = () => (
-    <CardHeader
-      className='ZCard-header'
-      avatar={avatar}
-      title={
-        <ZH2 className={cssJoinDefined('ZCard-header-heading', classes.title)} compact>
-          {heading}
-        </ZH2>
-      }
-      subheader={
-        <ZCaption className='ZCard-header-subheading' compact>
-          {subHeading}
-        </ZCaption>
-      }
-    />
-  );
-
-  const renderContent = () => {
-    const renderLoading = () => (
-      <ZSuspenseProgress className='ZCard-loading' name='card-loading' height={ZSizeFixed.Medium} />
-    );
-
-    return (
-      <CardContent className={cssJoinDefined('ZCard-content', classes.content)}>
-        {loading ? renderLoading() : children}
-      </CardContent>
-    );
-  };
-
-  const renderFooter = () => (footer ? <CardActions className='ZCard-footer'>{footer}</CardActions> : null);
-  */
 
   return (
     <z-card
@@ -170,19 +71,4 @@ export function ZCard(props: IZCard) {
       {footer && <div slot='footer'>{footer}</div>}
     </z-card>
   );
-  /*
-  return (
-    <Card
-      className={cssJoinDefined('ZCard-root', className, classes.root)}
-      elevation={5}
-      {...pickBy(props, (_, k) => startsWith(k, 'data-'))}
-      data-name={name}
-      data-fashion={fashion.name}
-    >
-      {renderHeader()}
-      {renderContent()}
-      {renderFooter()}
-    </Card>
-  );
-  */
 }
