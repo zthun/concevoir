b7b8c1995e2164f603717428245d84a9cb984017
Anthony Bonta <zthun.coding@gmail.com>
2024-04-12 16:34:01 -0500

    refactor: remove dependency on createStyleHook

--- packages/fashion-web/src/theme/theme-page.tsx
+++ packages/fashion-web/src/theme/theme-page.tsx
@@ -1,8 +1,10 @@
-import { ZBox, ZCard, ZGrid, ZH3, ZIconFontAwesome, ZParagraph, useFashionTheme } from '@zthun/fashion-react';
-import { ZSizeFixed } from '@zthun/fashion-tailor';
+import { ZBox, ZCard, ZH3, ZH4, ZIconFontAwesome, ZParagraph, ZStack } from '@zthun/fashion-react';
+import { ZGapSize, ZSizeFixed, ZSizeVoid } from '@zthun/fashion-tailor';
+import { ZFashionArea, ZFashionContrast, ZFashionName, ZFashionPriority, ZFashionSeverity } from '@zthun/fashion-theme';
+import { ZOrientation, ZQuadrilateralBuilder } from '@zthun/helpful-fn';
+import { noop, startCase } from 'lodash-es';
 import React from 'react';
 import { ZFashionRouteTheme } from '../routes.mjs';
-import { ZFashionColors } from './color/fashion-colors';
 
 /**
  * Represents the theme page.
@@ -10,7 +12,18 @@ import { ZFashionColors } from './color/fashion-colors';
  * @returns The JSX to render the theme page.
  */
 export function ZThemePage() {
-  const { primary, secondary, success, warning, error, info, light, dark, body, surface } = useFashionTheme();
+  const padding = new ZQuadrilateralBuilder(ZSizeFixed.Large).build();
+  const margin = new ZQuadrilateralBuilder<ZGapSize>(ZSizeVoid.None).bottom(ZSizeFixed.Medium).build();
+  const edge = new ZQuadrilateralBuilder(ZSizeFixed.Large).build();
+  const trim = new ZQuadrilateralBuilder('solid').build();
+
+  const renderFashion = (fashion: ZFashionName) => (
+    <ZBox padding={padding} edge={edge} trim={trim} fashion={fashion} margin={margin} onClick={noop}>
+      <ZStack orientation={ZOrientation.Horizontal} justifyContent='center'>
+        <ZH4 compact>{startCase(fashion)}</ZH4>
+      </ZStack>
+    </ZBox>
+  );
 
   return (
     <ZCard
@@ -26,20 +39,25 @@ export function ZThemePage() {
           A fashion theme describes the different makeup of page colors that are used across common places in a website.
         </ZParagraph>
 
-        <ZBox margin={{ bottom: ZSizeFixed.Medium }}>
-          <ZGrid columns={{ xl: '1fr 1fr 1fr', lg: '1fr 1fr', md: '1fr' }} gap={ZSizeFixed.Medium}>
-            <ZFashionColors fashion={primary} />
-            <ZFashionColors fashion={secondary} />
-            <ZFashionColors fashion={success} />
-            <ZFashionColors fashion={warning} />
-            <ZFashionColors fashion={error} />
-            <ZFashionColors fashion={info} />
-            <ZFashionColors fashion={light} />
-            <ZFashionColors fashion={dark} />
-            <ZFashionColors fashion={body} />
-            <ZFashionColors fashion={surface} />
-          </ZGrid>
-        </ZBox>
+        <ZH3>Priority</ZH3>
+        {renderFashion(ZFashionPriority.Primary)}
+        {renderFashion(ZFashionPriority.Secondary)}
+
+        <ZH3>Severity</ZH3>
+        {renderFashion(ZFashionSeverity.Success)}
+        {renderFashion(ZFashionSeverity.Warning)}
+        {renderFashion(ZFashionSeverity.Error)}
+        {renderFashion(ZFashionSeverity.Info)}
+
+        <ZH3>Contrast</ZH3>
+        {renderFashion(ZFashionContrast.Dark)}
+        {renderFashion(ZFashionContrast.Light)}
+        {renderFashion(ZFashionContrast.Opposite)}
+
+        <ZH3>Area</ZH3>
+        {renderFashion(ZFashionArea.Body)}
+        {renderFashion(ZFashionArea.Component)}
+        {renderFashion(ZFashionArea.Surface)}
       </ZBox>
     </ZCard>
   );
