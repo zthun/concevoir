cc4a7522d55ea5bc618b985a431c6b64713585c2
Anthony Bonta <zthun.coding@gmail.com>
2024-04-12 15:06:18 -0500

    refactor!: newspaper now uses device values for it's ranges rather than individual values for each.

--- packages/fashion-react/src/grid/newspaper.tsx
+++ packages/fashion-react/src/grid/newspaper.tsx
@@ -1,5 +1,6 @@
-import { ZSizeVaried } from '@zthun/fashion-tailor';
+import { ZDeviceBounds, ZDeviceValue, ZSizeVaried } from '@zthun/fashion-tailor';
 import { cssJoinDefined } from '@zthun/helpful-fn';
+import { mapValues } from 'lodash-es';
 import React from 'react';
 import { IZComponentHierarchy } from '../component/component-hierarchy.mjs';
 import { IZComponentStyle } from '../component/component-style.mjs';
@@ -10,45 +11,17 @@ export type ZNewspaperColumn = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export type ZNewspaperRange = [ZNewspaperColumn, ZNewspaperColumn];
 
 export interface IZNewspaper extends IZComponentStyle, IZComponentHierarchy {
-  GridProps?: Omit<
-    IZGrid,
-    | 'columns'
-    | 'columnsLg'
-    | 'columnsMd'
-    | 'columnsSm'
-    | 'columnsXs'
-    | 'children'
-    | 'className'
-    | 'name'
-    | 'width'
-    | 'widthLg'
-    | 'widthMd'
-    | 'widthSm'
-    | 'widthXs'
-  >;
-  range?: ZNewspaperRange;
-  rangeLg?: ZNewspaperRange;
-  rangeMd?: ZNewspaperRange;
-  rangeSm?: ZNewspaperRange;
-  rangeXs?: ZNewspaperRange;
+  GridProps?: Omit<IZGrid, 'columns' | 'children' | 'className' | 'width'>;
+  range?: ZDeviceValue<ZNewspaperRange>;
 }
 
 export function ZNewspaper(props: IZNewspaper) {
-  const {
-    GridProps,
-    className,
-    range = [1, 12],
-    rangeLg = range,
-    rangeMd = rangeLg,
-    rangeSm = rangeMd,
-    rangeXs = rangeSm,
-    children
-  } = props;
-  const [start, end] = range;
-  const [startLg, endLg] = rangeLg;
-  const [startMd, endMd] = rangeMd;
-  const [startSm, endSm] = rangeSm;
-  const [startXs, endXs] = rangeXs;
+  const { GridProps, className, range, children } = props;
+
+  const $range = new ZDeviceBounds<ZNewspaperRange>(range, [1, 12]).toDeviceMap();
+
+  const columnStart = mapValues($range, (r) => r[0]);
+  const columnEnd = mapValues($range, (r) => r[1] + 1);
 
   return (
     <ZGrid
@@ -57,18 +30,7 @@ export function ZNewspaper(props: IZNewspaper) {
       columns='1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr'
       width={ZSizeVaried.Full}
     >
-      <ZGridSpan
-        columnStart={start}
-        columnStartLg={startLg}
-        columnStartMd={startMd}
-        columnStartSm={startSm}
-        columnStartXs={startXs}
-        columnEnd={end + 1}
-        columnEndLg={endLg + 1}
-        columnEndMd={endMd + 1}
-        columnEndSm={endSm + 1}
-        columnEndXs={endXs + 1}
-      >
+      <ZGridSpan columnStart={columnStart} columnEnd={columnEnd}>
         {children}
       </ZGridSpan>
     </ZGrid>
