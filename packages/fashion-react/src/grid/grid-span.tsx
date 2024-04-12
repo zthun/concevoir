1c775c5881d6c1d323329d3af6c43ea4911ab9e1
Anthony Bonta <zthun.coding@gmail.com>
2024-04-12 15:05:52 -0500

    refactor!: grid span properties now use device values instead of individual values for each size
    ZGridSpan also now uses a web component under the hood

--- packages/fashion-react/src/grid/grid-span.tsx
+++ packages/fashion-react/src/grid/grid-span.tsx
@@ -1,105 +1,45 @@
-import { ZSizeFixed } from '@zthun/fashion-tailor';
+import { ZDeviceElement, ZGridSpanElement } from '@zthun/fashion-boutique';
+import { ZDeviceBounds, ZDeviceValue } from '@zthun/fashion-tailor';
 import { cssJoinDefined } from '@zthun/helpful-fn';
 import { Property } from 'csstype';
 import React from 'react';
 import { IZComponentHierarchy } from '../component/component-hierarchy.mjs';
 import { IZComponentStyle } from '../component/component-style.mjs';
-import { createStyleHook } from '../theme/styled';
+import { useWebComponent } from '../component/use-web-component.mjs';
 
-export interface IZGridSpan extends IZComponentHierarchy, IZComponentStyle {
-  columnStart?: Property.GridColumnStart;
-  columnStartLg?: Property.GridColumnStart;
-  columnStartMd?: Property.GridColumnStart;
-  columnStartSm?: Property.GridColumnStart;
-  columnStartXs?: Property.GridColumnStart;
-
-  columnEnd?: Property.GridColumnEnd;
-  columnEndLg?: Property.GridColumnEnd;
-  columnEndMd?: Property.GridColumnEnd;
-  columnEndSm?: Property.GridColumnEnd;
-  columnEndXs?: Property.GridColumnEnd;
-
-  rowStart?: Property.GridRowStart;
-  rowStartLg?: Property.GridRowStart;
-  rowStartMd?: Property.GridRowStart;
-  rowStartSm?: Property.GridRowStart;
-  rowStartXs?: Property.GridRowStart;
-
-  rowEnd?: Property.GridRowEnd;
-  rowEndLg?: Property.GridRowEnd;
-  rowEndMd?: Property.GridRowEnd;
-  rowEndSm?: Property.GridRowEnd;
-  rowEndXs?: Property.GridRowEnd;
+declare global {
+  namespace React.JSX {
+    interface IntrinsicElements {
+      ['z-grid-span']: ZGridSpanElement & any;
+    }
+  }
 }
 
-const useGridSpanStyles = createStyleHook(({ device }, props: IZGridSpan) => {
-  const {
-    columnStart,
-    columnStartLg = columnStart,
-    columnStartMd = columnStartLg,
-    columnStartSm = columnStartMd,
-    columnStartXs = columnStartSm,
-
-    columnEnd,
-    columnEndLg = columnEnd,
-    columnEndMd = columnEndLg,
-    columnEndSm = columnEndMd,
-    columnEndXs = columnEndSm,
-
-    rowStart,
-    rowStartLg = rowStart,
-    rowStartMd = rowStartLg,
-    rowStartSm = rowStartMd,
-    rowStartXs = rowStartSm,
-
-    rowEnd,
-    rowEndLg = rowEnd,
-    rowEndMd = rowEndLg,
-    rowEndSm = rowEndMd,
-    rowEndXs = rowEndSm
-  } = props;
-
-  return {
-    root: {
-      gridColumnStart: columnStart,
-      gridColumnEnd: columnEnd,
-      gridRowStart: rowStart,
-      gridRowEnd: rowEnd,
-
-      [device.break(ZSizeFixed.Large)]: {
-        gridColumnStart: columnStartLg,
-        gridColumnEnd: columnEndLg,
-        gridRowStart: rowStartLg,
-        gridRowEnd: rowEndLg
-      },
-
-      [device.break(ZSizeFixed.Medium)]: {
-        gridColumnStart: columnStartMd,
-        gridColumnEnd: columnEndMd,
-        gridRowStart: rowStartMd,
-        gridRowEnd: rowEndMd
-      },
-
-      [device.break(ZSizeFixed.Small)]: {
-        gridColumnStart: columnStartSm,
-        gridColumnEnd: columnEndSm,
-        gridRowStart: rowStartSm,
-        gridRowEnd: rowEndSm
-      },
-
-      [device.break(ZSizeFixed.ExtraSmall)]: {
-        gridColumnStart: columnStartXs,
-        gridColumnEnd: columnEndXs,
-        gridRowStart: rowStartXs,
-        gridRowEnd: rowEndXs
-      }
-    }
-  };
-});
+export interface IZGridSpan extends IZComponentHierarchy, IZComponentStyle {
+  columnStart?: ZDeviceValue<Property.GridColumnStart>;
+  columnEnd?: ZDeviceValue<Property.GridColumnEnd>;
+  rowStart?: ZDeviceValue<Property.GridRowStart>;
+  rowEnd?: ZDeviceValue<Property.GridRowEnd>;
+}
 
 export function ZGridSpan(props: IZGridSpan) {
-  const { className, children } = props;
-  const { classes } = useGridSpanStyles(props);
+  const { className, children, columnStart, columnEnd, rowStart, rowEnd } = props;
 
-  return <div className={cssJoinDefined('ZGridSpan-root', className, classes.root)}>{children}</div>;
+  const $cs = new ZDeviceBounds(columnStart, 'auto');
+  const $ce = new ZDeviceBounds(columnEnd, 'auto');
+  const $rs = new ZDeviceBounds(rowStart, 'auto');
+  const $re = new ZDeviceBounds(rowEnd, 'auto');
+
+  useWebComponent(ZDeviceElement);
+  useWebComponent(ZGridSpanElement);
+
+  return (
+    <z-grid-span className={cssJoinDefined(className)}>
+      <z-device name='column-start' xl={$cs.xl()} lg={$cs.lg()} md={$cs.md()} sm={$cs.sm()} xs={$cs.xs()} />
+      <z-device name='column-end' xl={$ce.xl()} lg={$ce.lg()} md={$ce.md()} sm={$ce.sm()} xs={$ce.xs()} />
+      <z-device name='row-start' xl={$rs.xl()} lg={$rs.lg()} md={$rs.md()} sm={$rs.sm()} xs={$rs.xs()} />
+      <z-device name='row-end' xl={$re.xl()} lg={$re.lg()} md={$re.md()} sm={$re.sm()} xs={$re.xs()} />
+      {children}
+    </z-grid-span>
+  );
 }
