2e1d1202a8a137502482ab5d0a043722e078340d
Anthony Bonta <zthun.coding@gmail.com>
2024-04-14 17:14:21 -0500

    refactor: component range has been moved to boutique

--- packages/fashion-boutique/src/index.ts
+++ packages/fashion-boutique/src/index.ts
@@ -19,6 +19,7 @@ export * from './component/component-height.mjs';
 export * from './component/component-loading.mjs';
 export * from './component/component-name.mjs';
 export * from './component/component-orientation.mjs';
+export * from './component/component-range.mjs';
 export * from './component/component-required.mjs';
 export * from './component/component-width.mjs';
 export * from './full-screen/full-screen-element.mjs';
