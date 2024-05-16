085a495c773eb1082ae4bf393757a00a36c9b226
Anthony Bonta <zthun.coding@gmail.com>
2024-05-16 11:03:11 -0500

    refactor: drawer signature changed to use a dialog in the shadow dom

--- packages/fashion-react/src/dialog/drawer.tsx
+++ packages/fashion-react/src/dialog/drawer.tsx
@@ -1,9 +1,17 @@
 import { IZComponentHierarchy, ZDrawerElement } from '@zthun/fashion-boutique';
 import { ZSideAnchor, cssJoinDefined } from '@zthun/helpful-fn';
-import React, { ReactNode, useRef } from 'react';
+import React, { ReactNode, useCallback, useEffect, useRef } from 'react';
 import { IZComponentStyle } from '../component/component-style.mjs';
 import { useWebComponent } from '../component/use-web-component.mjs';
-import { IZDialogProps, useDialog } from './use-dialog';
+import { IZDialogProps } from './use-dialog';
+
+declare global {
+  namespace React.JSX {
+    interface IntrinsicElements {
+      ['z-drawer']: ZDrawerElement & any;
+    }
+  }
+}
 
 /**
  * Represents props for the drawer.
@@ -22,20 +30,32 @@ export interface IZDrawer extends IZComponentHierarchy<ReactNode>, IZComponentSt
  *        The JSX to render the component.
  */
 export function ZDrawer(props: IZDrawer) {
-  const { className, children, anchor } = props;
+  const { className, children, anchor, onClose, open } = props;
   const drawer = useRef<ZDrawerElement>(null);
-  useDialog(drawer.current, props);
   useWebComponent(ZDrawerElement);
 
+  const onClosed = useCallback(() => onClose?.call(null), [onClose]);
+
+  useEffect(() => {
+    drawer.current?.removeEventListener('close', onClosed);
+    drawer.current?.addEventListener('close', onClosed);
+
+    return () => {
+      drawer.current?.removeEventListener('close', onClosed);
+    };
+  }, [drawer.current, onClosed]);
+
+  useEffect(() => {
+    if (open) {
+      drawer.current?.open?.call(drawer.current);
+    } else {
+      drawer.current?.close?.call(drawer.current);
+    }
+  }, [drawer.current, open]);
+
   return (
-    <dialog
-      is='z-drawer'
-      // @ts-expect-error React 18 needs to use class for web components
-      class={cssJoinDefined(className)}
-      ref={drawer}
-      data-anchor={anchor}
-    >
+    <z-drawer class={cssJoinDefined(className)} ref={drawer} anchor={anchor}>
       {children}
-    </dialog>
+    </z-drawer>
   );
 }
