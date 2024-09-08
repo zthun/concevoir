2b01c8f8d7624dd6855e8788f7854043c0ee544f
Anthony Bonta <zthun.coding@gmail.com>
2024-09-08 10:49:57 -0500

    chore: lint cleanup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
 const config = require("@zthun/lint-janitor-config/eslint-react");
-config.rules = config.rules || {};
-config.rules["react/react-in-jsx-scope"] = "off";
 config.env = { node: true };
 module.exports = config;
