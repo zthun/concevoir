813a6e202b08d65dafdcf4148b5cf59237d39f21
Anthony Bonta <zthun.coding@gmail.com>
2024-09-08 10:52:49 -0500

    chore: export color picker

--- packages/fashion-theme/src/index.ts
+++ packages/fashion-theme/src/index.ts
@@ -4,6 +4,7 @@ export * from "./color/color.mjs";
 export * from "./color/hex.mjs";
 export * from "./color/hsl.mjs";
 export * from "./color/rgb.mjs";
+export * from "./fashion/color-picker.mjs";
 export * from "./fashion/fashion.mjs";
 export * from "./theme/dark.mjs";
 export * from "./theme/fashion-theme.mjs";
