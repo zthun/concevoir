9fc272ab87d1d4bcad41e509fad2ae911283cd2e
Anthony Bonta <zthun.coding@gmail.com>
2024-09-07 21:47:28 -0500

    refactor: alert no longer uses mui alert

--- packages/fashion-boutique/src/alert/alert.tsx
+++ packages/fashion-boutique/src/alert/alert.tsx
@@ -1,50 +1,74 @@
-import { Alert, AlertTitle } from "@mui/material";
+import { css } from "@emotion/css";
 import { ZSizeFixed } from "@zthun/fashion-tailor";
 import { cssJoinDefined, firstDefined } from "@zthun/helpful-fn";
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import { IZComponentAvatar } from "../component/component-avatar.mjs";
 import { IZComponentFashion } from "../component/component-fashion.mjs";
 import { IZComponentHeading } from "../component/component-heading.mjs";
 import { IZComponentName } from "../component/component-name.mjs";
 import { IZComponentStyle } from "../component/component-style.mjs";
-import { useFashionTheme } from "../theme/fashion.mjs";
-import { createStyleHook } from "../theme/styled";
+import { useFashionTailor, useFashionTheme } from "../theme/fashion.mjs";
 
 export interface IZAlert
-  extends Omit<IZComponentHeading, "subheader">,
+  extends Omit<IZComponentHeading, "subHeading">,
     IZComponentName,
     IZComponentStyle,
     IZComponentFashion,
     IZComponentAvatar {
-  message: ReactNode;
+  message?: ReactNode;
 }
 
-const useAlertStyles = createStyleHook(({ theme, tailor }, props: IZAlert) => {
+const useAlertStyles = (props: IZAlert) => {
+  const theme = useFashionTheme();
+  const tailor = useFashionTailor();
   const { primary } = theme;
   const { fashion = primary } = props;
 
   const border = firstDefined(fashion.idle.main, fashion.idle.border);
+  const boxWidth = tailor.thickness(ZSizeFixed.ExtraSmall);
+  const boxShadow = fashion.idle.border;
+  const px = tailor.gap(ZSizeFixed.Small);
+  const py = tailor.gap(ZSizeFixed.ExtraSmall);
 
-  return {
-    root: {
-      display: "flex",
-      alignItems: "center",
-      backgroundColor: fashion.idle.main,
-      color: fashion.idle.contrast,
-      boxShadow: `0 0 0 ${tailor.thickness()} ${border}`,
-      border: `${tailor.thickness()} double ${border}`,
+  return css`
+    &.ZAlert-root {
+      align-items: center;
+      background: ${fashion.idle.main};
+      border-color: ${fashion.idle.border};
+      border-radius: 0.25rem;
+      border-style: double;
+      box-shadow: 0 0 0 ${boxWidth} ${boxShadow};
+      color: ${fashion.idle.contrast};
+      display: grid;
+      grid-template-columns: auto auto 1fr;
+      grid-template-areas:
+        "avatar heading    ."
+        "avatar subheading ."
+        "avatar message    ."
+        "avatar footing    .";
+      padding: ${px} ${py};
+    }
 
-      ".MuiAlert-icon": {
-        color: "inherit",
-      },
-    },
+    .ZAlert-avatar {
+      grid-area: avatar;
+      margin-right: ${tailor.gap(ZSizeFixed.Medium)};
+    }
 
-    heading: {
-      margin: 0,
-      marginBottom: tailor.gap(ZSizeFixed.ExtraSmall),
-    },
-  };
-});
+    .ZAlert-heading {
+      grid-area: heading;
+      margin-bottom: ${tailor.gap(ZSizeFixed.Small)};
+    }
+
+    .ZAlert-message {
+      grid-area: message;
+    }
+
+    .ZAlert-footing {
+      grid-area: footing;
+      margin-top: ${tailor.gap(ZSizeFixed.Small)};
+    }
+  `;
+};
 
 export function ZAlert(props: IZAlert) {
   const { primary } = useFashionTheme();
@@ -52,35 +76,22 @@ export function ZAlert(props: IZAlert) {
     heading,
     name,
     className,
-    message,
     avatar,
+    message,
     fashion = primary,
   } = props;
-  const { classes } = useAlertStyles(props);
 
-  const renderHeading = () =>
-    heading ? (
-      <AlertTitle className={cssJoinDefined("ZAlert-heading", classes.heading)}>
-        {heading}
-      </AlertTitle>
-    ) : null;
-
-  const renderIcon = () =>
-    avatar ? (
-      <div className={cssJoinDefined("ZAlert-avatar")}>{avatar}</div>
-    ) : (
-      " "
-    );
+  const _className = useAlertStyles(props);
 
   return (
-    <Alert
-      className={cssJoinDefined("ZAlert-root", className, classes.root)}
-      icon={renderIcon()}
+    <div
+      className={cssJoinDefined("ZAlert-root", className, _className)}
       data-name={name}
       data-fashion={fashion.name}
     >
-      {renderHeading()}
-      <div className={cssJoinDefined("ZAlert-message")}>{message}</div>
-    </Alert>
+      {avatar && <div className="ZAlert-avatar">{avatar}</div>}
+      {heading && <div className="ZAlert-heading">{heading}</div>}
+      <div className={"ZAlert-message"}>{message}</div>
+    </div>
   );
 }
