1d93218bfb6591198ab3e045c2459f9309bca2ba
Anthony Bonta <zthun.coding@gmail.com>
2024-09-07 21:53:19 -0500

    build: turn off react-in-jsx-scope
    This isn't needed since we are switching to react-jsx

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
 const config = require("@zthun/lint-janitor-config/eslint-react");
+config.rules = config.rules || {};
+config.rules["react/react-in-jsx-scope"] = "off";
 config.env = { node: true };
 module.exports = config;
