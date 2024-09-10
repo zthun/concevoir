6e9229d538e08a37e9d58d8b7a30c8a2b4a3c388
Anthony Bonta <zthun.coding@gmail.com>
2024-09-10 05:18:16 -0500

    refactor: youtube video no longer uses createStyleHook

--- packages/fashion-boutique/src/you-tube/you-tube-video.tsx
+++ packages/fashion-boutique/src/you-tube/you-tube-video.tsx
@@ -1,39 +1,15 @@
+import { css } from "@emotion/css";
 import { cssJoinDefined } from "@zthun/helpful-fn";
 import { ZUrlBuilder, ZYouTubeApi } from "@zthun/webigail-url";
 import { IZComponentName } from "../component/component-name.mjs";
 import { IZComponentStyle } from "../component/component-style.mjs";
-import { createStyleHook } from "../theme/styled";
 
 export interface IZYouTubeVideo extends IZComponentName, IZComponentStyle {
   identity: string;
 }
 
-const useYouTubeVideoStyles = createStyleHook(() => {
-  return {
-    root: {
-      overflow: "hidden",
-      // This isn't obvious why, but if you're seeing this, it's to force
-      // a 16x9 aspect ratio.  See https://css-tricks.com/fluid-width-video/
-      // for more information.
-      paddingBottom: "56.25%",
-      position: "relative",
-      height: 0,
-
-      iframe: {
-        left: 0,
-        top: 0,
-        height: "100%",
-        width: "100%",
-        position: "absolute",
-        border: 0,
-      },
-    },
-  };
-});
-
 export function ZYouTubeVideo(props: IZYouTubeVideo) {
   const { identity, className, name } = props;
-  const { classes } = useYouTubeVideoStyles();
   const src = new ZUrlBuilder().youTube(ZYouTubeApi.Embed, identity).build();
   const allow = [
     "accelerometer",
@@ -44,9 +20,32 @@ export function ZYouTubeVideo(props: IZYouTubeVideo) {
     "picture-in-picture",
   ].join(";");
 
+  // This isn't obvious why, but if you're seeing this, it's to force
+  // a 16x9 aspect ratio.  See https://css-tricks.com/fluid-width-video/
+  // for more information.
+  const padding = "56.25%";
+
+  const _className = css`
+    & {
+      overflow: hidden;
+      padding-bottom: ${padding};
+      position: relative;
+      height: 0;
+    }
+
+    iframe {
+      left: 0;
+      top: 0;
+      height: 100%;
+      width: 100%;
+      position: absolute;
+      border: 0;
+    }
+  `;
+
   return (
     <div
-      className={cssJoinDefined("ZYouTubeVideo-root", className, classes.root)}
+      className={cssJoinDefined("ZYouTubeVideo-root", className, _className)}
       data-identity={identity}
       data-name={name}
     >
