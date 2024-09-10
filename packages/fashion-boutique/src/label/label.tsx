babfef20439239c544396afdc72d4f5d2b0bfe14
Anthony Bonta <zthun.coding@gmail.com>
2024-09-10 05:06:02 -0500

    refactor: label no longer uses createStyleHook

--- packages/fashion-boutique/src/label/label.tsx
+++ packages/fashion-boutique/src/label/label.tsx
@@ -1,22 +1,10 @@
+import { css } from "@emotion/css";
 import { ZSizeFixed } from "@zthun/fashion-tailor";
 import { cssJoinDefined } from "@zthun/helpful-fn";
 import { IZComponentHierarchy } from "../component/component-hierarchy.mjs";
 import { IZComponentRequired } from "../component/component-required.mjs";
 import { IZComponentStyle } from "../component/component-style.mjs";
-import { createStyleHook } from "../theme/styled";
-
-const useLabelStyles = createStyleHook(({ theme, tailor }) => ({
-  root: {
-    display: "block",
-    fontWeight: "bold",
-
-    '&[data-required="true"]::after': {
-      content: '"*"',
-      color: theme.error.idle.main,
-      marginLeft: tailor.thickness(ZSizeFixed.Medium),
-    },
-  },
-}));
+import { useFashionTailor, useFashionTheme } from "../theme/fashion.mjs";
 
 export interface IZLabel
   extends IZComponentHierarchy,
@@ -26,12 +14,26 @@ export interface IZLabel
 }
 
 export function ZLabel(props: IZLabel) {
+  const { error } = useFashionTheme();
+  const tailor = useFashionTailor();
   const { children, className, required, htmlFor } = props;
-  const { classes } = useLabelStyles();
+
+  const _className = css`
+    & {
+      display: block;
+      font-weight: bold;
+    }
+
+    &[data-required="true"]::after {
+      content: "*";
+      color: ${error.idle.main};
+      margin-left: ${tailor.thickness(ZSizeFixed.Medium)};
+    }
+  `;
 
   return (
     <label
-      className={cssJoinDefined("ZLabel-root", className, classes.root)}
+      className={cssJoinDefined("ZLabel-root", className, _className)}
       htmlFor={htmlFor}
       data-required={required}
     >
