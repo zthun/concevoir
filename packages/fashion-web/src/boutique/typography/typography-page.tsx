0cebbc9da7e92e6de3658b5141cb7f05c8631eb0
Anthony Bonta <zthun.coding@gmail.com>
2024-09-10 00:59:46 -0500

    refactor: remove text color in lieu of individual fashion

--- packages/fashion-web/src/boutique/typography/typography-page.tsx
+++ packages/fashion-web/src/boutique/typography/typography-page.tsx
@@ -13,7 +13,6 @@ import {
   ZOverline,
   ZParagraph,
   ZSubtitle,
-  ZTextColor,
 } from "@zthun/fashion-boutique";
 import { ZSizeFixed } from "@zthun/fashion-tailor";
 import { ZFashionRouteTypography } from "../../routes.mjs";
@@ -50,46 +49,43 @@ export function ZTypographyPage() {
         devices, you want the page to respond to changes made.
       </ZParagraph>
 
-      <ZTextColor fashion={fashion} name="color">
-        <ZH1>Headline 1</ZH1>
-        <ZH2>Headline 2</ZH2>
-        <ZH3>Headline 3</ZH3>
-        <ZH4>Headline 4</ZH4>
-        <ZH5>Headline 5</ZH5>
-        <ZH6>Headline 6</ZH6>
+      <ZH1 fashion={fashion}>Headline 1</ZH1>
+      <ZH2 fashion={fashion}>Headline 2</ZH2>
+      <ZH3 fashion={fashion}>Headline 3</ZH3>
+      <ZH4 fashion={fashion}>Headline 4</ZH4>
+      <ZH5 fashion={fashion}>Headline 5</ZH5>
+      <ZH6 fashion={fashion}>Headline 6</ZH6>
 
-        <hr />
-        <ZParagraph>
-          Paragraph: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec
-          dui nunc mattis enim. Velit laoreet id donec ultrices tincidunt arcu.
-          In mollis nunc sed id semper. Aliquet lectus proin nibh nisl
-          condimentum id venenatis. Convallis aenean et tortor at risus.
-          Fringilla phasellus faucibus scelerisque eleifend. Eu sem integer
-          vitae justo eget magna fermentum iaculis eu. Enim sit amet venenatis
-          urna cursus. Nisl suscipit adipiscing bibendum est ultricies integer.
-          In hendrerit gravida rutrum quisque non tellus. Adipiscing tristique
-          risus nec feugiat in fermentum posuere urna nec. Quam adipiscing vitae
-          proin sagittis nisl rhoncus.
-        </ZParagraph>
+      <hr />
+      <ZParagraph fashion={fashion}>
+        Paragraph: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec dui
+        nunc mattis enim. Velit laoreet id donec ultrices tincidunt arcu. In
+        mollis nunc sed id semper. Aliquet lectus proin nibh nisl condimentum id
+        venenatis. Convallis aenean et tortor at risus. Fringilla phasellus
+        faucibus scelerisque eleifend. Eu sem integer vitae justo eget magna
+        fermentum iaculis eu. Enim sit amet venenatis urna cursus. Nisl suscipit
+        adipiscing bibendum est ultricies integer. In hendrerit gravida rutrum
+        quisque non tellus. Adipiscing tristique risus nec feugiat in fermentum
+        posuere urna nec. Quam adipiscing vitae proin sagittis nisl rhoncus.
+      </ZParagraph>
 
-        <hr />
-        <ZSubtitle>
-          Subtitle: Pulvinar elementum integer enim neque volutpat ac.
-          Ullamcorper a lacus vestibulum sed. Risus pretium quam vulputate
-          dignissim suspendisse in est ante.
-        </ZSubtitle>
+      <hr />
+      <ZSubtitle fashion={fashion}>
+        Subtitle: Pulvinar elementum integer enim neque volutpat ac. Ullamcorper
+        a lacus vestibulum sed. Risus pretium quam vulputate dignissim
+        suspendisse in est ante.
+      </ZSubtitle>
 
-        <hr />
-        <ZCaption>
-          Caption: Viverra maecenas accumsan lacus vel facilisis volutpat est.
-        </ZCaption>
+      <hr />
+      <ZCaption fashion={fashion}>
+        Caption: Viverra maecenas accumsan lacus vel facilisis volutpat est.
+      </ZCaption>
 
-        <hr />
-        <ZOverline>
-          Overline: Pretium quam vulputate dignissim suspendisse.
-        </ZOverline>
-      </ZTextColor>
+      <hr />
+      <ZOverline fashion={fashion}>
+        Overline: Pretium quam vulputate dignissim suspendisse.
+      </ZOverline>
 
       <ZH3>Options</ZH3>
       <ZGrid gap={ZSizeFixed.Medium}>
