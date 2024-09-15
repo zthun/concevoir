62aa597e5d20980d5980b28edefe7fa8a0b167e1
Anthony Bonta <zthun.coding@gmail.com>
2024-09-15 00:30:50 -0500

    refactor: give margins with support for compact (no-margin)

--- packages/fashion-boutique/src/divider/divider.tsx
+++ packages/fashion-boutique/src/divider/divider.tsx
@@ -1,15 +1,26 @@
+import { css } from "@emotion/css";
 import { cssJoinDefined } from "@zthun/helpful-fn";
 import { IZComponentStyle } from "../component/component-style.mjs";
+import { useFashionTailor } from "../theme/fashion.mjs";
 
-export interface IZDivider extends IZComponentStyle {}
+export interface IZDivider extends IZComponentStyle {
+  compact?: boolean;
+}
 
 export function ZDivider(props: IZDivider) {
-  const { className } = props;
+  const tailor = useFashionTailor();
+  const { className, compact } = props;
+
+  const _className = css`
+    & {
+      color: inherit;
+      margin: ${compact ? 0 : tailor.gap()};
+      margin-left: 0;
+      margin-right: 0;
+    }
+  `;
 
   return (
-    <hr
-      className={cssJoinDefined("ZDivider-root", className)}
-      style={{ color: "inherit" }}
-    />
+    <hr className={cssJoinDefined("ZDivider-root", _className, className)} />
   );
 }
