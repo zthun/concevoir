fd92cd9a4405fb4bcd68c3ff7b4b868efaa51fd1
Anthony Bonta <zthun.coding@gmail.com>
2024-10-27 14:37:35 -0500

    refactor: compact list item now compacts the vertical gap

--- packages/fashion-boutique/src/list/list-item.tsx
+++ packages/fashion-boutique/src/list/list-item.tsx
@@ -2,6 +2,7 @@ import { css } from "@emotion/css";
 import { ZSizeFixed } from "@zthun/fashion-tailor";
 import { cssJoinDefined } from "@zthun/helpful-fn";
 import { Property } from "csstype";
+import { IZComponentCompact } from "../component/component-compact.mjs";
 import { IZComponentDomEvents } from "../component/component-dom-events.mjs";
 import { IZComponentHierarchy } from "../component/component-hierarchy.mjs";
 import { IZComponentName } from "../component/component-name.mjs";
@@ -12,9 +13,9 @@ export interface IZListItem
   extends IZComponentName,
     IZComponentStyle,
     IZComponentHierarchy,
+    IZComponentCompact,
     IZComponentDomEvents<HTMLLIElement> {
   cursor?: Property.Cursor;
-  compact?: boolean;
   interactive?: boolean;
 }
 
@@ -23,9 +24,11 @@ export function ZListItem(props: IZListItem) {
   const { children, className, compact, cursor, interactive, name, ...dom } =
     props;
 
-  const py = tailor.gap(ZSizeFixed.Small);
+  const py = compact
+    ? tailor.gap(ZSizeFixed.ExtraSmall)
+    : tailor.gap(ZSizeFixed.Small);
   const px = compact
-    ? tailor.gap(ZSizeFixed.Small)
+    ? tailor.gap(ZSizeFixed.ExtraSmall)
     : tailor.gap(ZSizeFixed.Large);
 
   const _className = css`
