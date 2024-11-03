c52a24cc9d29bb66b76f0aa8a32d94fa798a5921
Anthony Bonta <zthun.coding@gmail.com>
2024-11-03 11:04:07 -0600

    fix: styled will now properly revert the global styles when switching themes

--- packages/fashion-boutique/src/theme/styled.tsx
+++ packages/fashion-boutique/src/theme/styled.tsx
@@ -1,9 +1,43 @@
-import { injectGlobal } from "@emotion/css";
-import { cssJoinDefined } from "@zthun/helpful-fn";
-import { useEffect } from "react";
+import { serializeStyles } from "@emotion/serialize";
+import { StyleSheet } from "@emotion/sheet";
+import { css, cssJoinDefined } from "@zthun/helpful-fn";
+import { useEffect, useRef } from "react";
+import { compile, middleware, rulesheet, serialize, stringify } from "stylis";
 import { IZComponentHierarchy } from "../component/component-hierarchy.mjs";
 import { IZComponentStyle } from "../component/component-style.mjs";
 import { useFashionTheme } from "./fashion.mjs";
+
+/**
+ * Adds global css to the head.
+ *
+ * @param css -
+ *        The global css to inject.
+ */
+export function useGlobalCss(css: string) {
+  const flush = useRef<() => void>();
+
+  useEffect(() => {
+    flush.current?.call(null);
+    const { name, styles } = serializeStyles(css as any);
+    const sheet = new StyleSheet({
+      key: `global-${name}`,
+      container: document.head,
+    });
+    const stylis = (styles: any) =>
+      serialize(
+        compile(styles),
+        middleware([
+          stringify,
+          rulesheet((rule) => {
+            sheet.insert(rule);
+          }),
+        ]),
+      );
+    stylis(styles);
+    flush.current = () => sheet.flush();
+    return () => flush.current?.call(null);
+  }, [css]);
+}
 
 /**
  * Properties for the styled application.
@@ -19,17 +53,15 @@ export function ZStyled(props: IZStyled) {
   const { children, className } = props;
   const { body } = useFashionTheme();
 
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const _ = injectGlobal`
+  useGlobalCss(css`
     body {
       background-color: ${body.idle.main};
       color: ${body.idle.contrast};
       margin: 0;
       position: relative;
       height: 100vh;
-    }`;
-  }, []);
+    }
+  `);
 
   return (
     <div className={cssJoinDefined("ZStyled-root", className)}>{children}</div>
