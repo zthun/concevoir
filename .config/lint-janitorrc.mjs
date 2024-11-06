0e747171082934ad91cae0b9303f75bb70c54f86
Anthony Bonta <zthun.coding@gmail.com>
2024-11-06 08:36:49 -0600

    build: lint rules

--- .config/lint-janitorrc.mjs
+++ .config/lint-janitorrc.mjs
@@ -3,22 +3,21 @@ const generated = [
   "packages/**/dist/**",
   "packages/**/docs/**",
   "packages/**/stats/**",
-  "node_modules/**",
-  "packages/**/node_modules/**",
+  "**/node_modules/**",
   "package-lock.json",
   ".yarnrc.yml",
+  ".config/cspell.json",
+  "lerna.json",
 ];
-const partialGenerated = ["cspell.json", "lerna.json"];
 
 const esFiles = [
-  "*.cjs",
-  "*.ts",
-  "packages/**/src/**/*.ts",
-  "packages/**/src/**/*.tsx",
+  "*.{cjs,mjs,js,ts,mts,tsx,jsx}",
+  "packages/**/src/**/*.{cjs,mjs,js,ts,mts,tsx,jsx}",
+  ".config/**/*.{cjs,mjs,js,ts,mts,tsx,jsx}",
 ];
 const htmlFiles = ["packages/**/*.html"];
 const markdownFiles = ["*.md", "packages/**/*.md"];
-const jsonFiles = ["*.json", "packages/**/*.json"];
+const jsonFiles = ["*.json", "packages/**/*.json", ".config/**/*.json"];
 const yamlFiles = [".circleci/config.yml"];
 const prettyFiles = []
   .concat(esFiles)
@@ -38,8 +37,8 @@ const htmlFilesExclude = generated;
 const markdownFilesExclude = generated;
 const jsonFilesExclude = generated;
 const yamlFilesExclude = generated;
-const prettyFilesExclude = generated.concat(partialGenerated);
-const spellingFilesExclude = generated.concat(partialGenerated);
+const prettyFilesExclude = generated;
+const spellingFilesExclude = generated;
 
 export default {
   esFiles,
