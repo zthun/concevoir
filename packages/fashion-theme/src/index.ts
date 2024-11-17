fb913ed83bc165cfedfe5f66a6373cc293736def
Anthony Bonta <zthun.coding@gmail.com>
2024-11-17 14:36:48 -0600

    fix: export fashion state

--- packages/fashion-theme/src/index.ts
+++ packages/fashion-theme/src/index.ts
@@ -5,6 +5,7 @@ export * from "./color/hex.mjs";
 export * from "./color/hsl.mjs";
 export * from "./color/rgb.mjs";
 export * from "./fashion/color-picker.mjs";
+export * from "./fashion/fashion-state.mjs";
 export * from "./fashion/fashion.mjs";
 export * from "./theme/dark.mjs";
 export * from "./theme/fashion-theme.mjs";
